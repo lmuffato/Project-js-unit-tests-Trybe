067aeed2613195eef1fe6385653c20fb6121df08
Lucas Muffato <lmiremuffato@gmail.com>
2021-03-26 19:53:32 -0300

    requisito 1

--- src/average.js
+++ src/average.js
@@ -10,9 +10,19 @@
     - average([2, 2]) // Retorno: 2;
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
-    - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let sum = 0;
+  if (array.length < 1) {
+    return undefined;
+  }
+  for(index = 0; index < array.length; index += 1) {
+    if ( typeof array[index] !== 'number') {
+      return undefined;
+      } sum += array[index];}      
+    sum = Math.round(sum/(array.length));
+  return sum;
+}
 
-module.exports = average;
+module.exports = average
