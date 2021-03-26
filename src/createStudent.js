ae6a583a16cedbc19ecc0f499dc422b3d682707f
Iago de Paula <iago.pferreiravr@gmail.com>
2021-03-26 17:58:37 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const student = {
+    name: nome,
+    feedback() {
+      return 'Eita pessoa boa!';
+    }
+  }
+  return student
+};
 
 module.exports = createStudent;
