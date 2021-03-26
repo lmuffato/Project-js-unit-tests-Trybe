db969756cafd54408fcec45467256ee0841bd170
Bruno Mendes <mendessbruno@gmail.com>
2021-03-26 19:40:17 -0300

    resolvidos mais alguns problemas de lint

--- src/vqv.js
+++ src/vqv.js
@@ -19,12 +19,11 @@
 const vqv = (nome, idade) => {
   if (typeof nome === 'undefined') {
     return undefined;
-  } else {
-    return `Oi, meu nome é ${nome}!
-    Tenho ${idade} anos,
-    trabalho na Trybe e mando muito em programação!
-    #VQV!`
-  }
-}
+  } 
+  return `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`;
+};
 
 module.exports = vqv;
