b5c99faf6c96594f88be003afa50eed5f8adafc3
lucas-paz <lucas.hpaz@outlook.com>
2021-03-26 19:05:55 -0300

    remove as linha assert.fail()

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -18,7 +18,6 @@ const numbers = require('../src/numbers');
 
 describe('#numbers', () => {
   it('should return an array and return if it has only numbers or not', () => {
-    assert.fail();
     assert.deepStrictEqual(numbers([1, 2, 3, 4, 5]), true);
     assert.deepStrictEqual(numbers([1, 2, '3', 4, 5]), false);
     assert.deepStrictEqual(numbers([1, 'a', 3]), false);
