16715dff2cb2e9a3a9e4d54919408600cdc5b5d7
Alexandre Oliveira <alexandremolive@gmail.com>
2021-03-26 17:32:49 -0300

    exercicio 3

---
+++ testes.js
@@ -0,0 +1,9 @@
+const vqv = (nome, idade) => {
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
+  return `'Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!'`;
+};
