a0b12d90274c9418cb3557cb2f8ad33b445b204e
guilhermemd <guilherme.md@gmail.com>
2021-03-26 18:58:52 -0300

    teste de conflito

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -39,5 +39,5 @@ describe('#circle', () => {
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     assert.deepStrictEqual((circle(3)), { radius: 3, area: 28.259999999999998, circumference: 18.84 })
   });
-  
+  //teste de push
 });
