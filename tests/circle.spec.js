84ee3537879f667f069e58910d45c33a8caea84c
Carol <carolinavpinheiro@gmail.com>
2021-03-26 19:04:59 -0300

    Adicionando alteraçoes para resolver problemas do eslint

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,7 +25,7 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();
+    assert.strictEqual(typeof(circle({})), 'object');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
     // Teste se o objeto retornado tem 3 entradas.
