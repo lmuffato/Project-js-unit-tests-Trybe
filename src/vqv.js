1d9cffaa0cc9675914fcd54b22ec52d36b9c5919
mallmann02 <leomallmann020302@gmail.com>
2021-03-26 19:07:31 -0300

    Refatora função vqv

--- src/vqv.js
+++ src/vqv.js
@@ -16,11 +16,10 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {
-  return `'Oi, meu nome é ${nome}!
+const vqv = (nome, idade) =>
+  `'Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!'`;
-};
 
 module.exports = vqv;
