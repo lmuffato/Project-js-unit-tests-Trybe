18694db32939b2e0b15a50fd559fbbc66923094b
Luiz Felipe Araujo <thelastnailmusic@gmail.com>
2021-03-26 17:46:50 -0300

    Desafio 5 Ok

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,15 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const estudante = {
+    name: name,
+    feedback: () => {return 'Eita pessoa boa!'}
+  };
+  
+  return estudante;
+};
+
+console.log(createStudent('Luiz Felipe'));
 
 module.exports = createStudent;
