cea3f19dd49035b4b48870275da4a058c13fdebe
Maria Baeta <m.martinsdg@gmail.com>
2021-03-26 16:34:08 -0300

    Atende ao requisito 1

--- src/average.js
+++ src/average.js
@@ -12,8 +12,24 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
+const average = (array) => {
+  let sum = 0;
+  let lengthArr = 0;
   
-};
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if ((typeof(array[index]) !== 'number')) {
+      return undefined;
+    } else {
+        sum += array[index];
+        lengthArr = array.length
+    }
+  }  
+  return Math.round(sum / lengthArr)
+  }  
+console.log(average([]));
+
 
 module.exports = average;
