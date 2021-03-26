7c59e12e496100b8f305c824b82740f4d122793e
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-03-26 16:40:42 -0300

    Requisito 1: Average

--- src/average.js
+++ src/average.js
@@ -12,9 +12,25 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
+const average = (array) => {
+  let media;
+  let soma=0;
 
-  
+  if(array.length === 0){
+    return undefined;
+  }
+
+  for(let index=0; index < array.length; index+=1){
+    if(typeof array[index] !== 'number'){
+      return undefined;
+    }
+   
+    soma = soma + array[index];
+
+  }
+  media=soma/array.length;
+
+  return Math.round(media);
 };
 
 module.exports = average;
