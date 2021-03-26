3a7bdd2412308372e5c79d9ab32c7436f8d58bc0
Guilherme Dugaich de Oliveira <guilherme.dugaich@gmail.com>
2021-03-26 18:23:04 -0300

    requisito 6 - implementar testes para funcao productDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,20 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof(productDetails()), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('uno', 'dos').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof(productDetails()[0]), 'object');
+    assert.strictEqual(typeof(productDetails()[1]), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notStrictEqual(productDetails()[0], productDetails()[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    const result = productDetails('uno', 'dos');
+    const productId0 = result[0].details.productId
+    const productId1 = result[1].details.productId
+    assert.strictEqual(productId0.substring(productId0.length - 3), productId1.substring(productId1.length - 3))
   });
 });
