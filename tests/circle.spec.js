71993aab2ab82c8024f008f37f7f8fdbe21bfdd8
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-03-26 17:31:58 -0300

    Implemente os casos de teste para a função circle done

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,18 +25,11 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se circle retorna um objeto.
     assert.strictEqual(typeof circle, 'function');
-    // Teste se o objeto retornado tem 3 entradas.
-    // assert.strictEqual()
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(Object.entries(circle(2)).length, 3);
     assert.strictEqual(circle(), undefined);
-    // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.strictEqual(circle(2).circumference, 12.56)
-    // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     assert.strictEqual(circle(3).area, 28.26);
-    // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     assert.deepStrictEqual(circle(3), {radius: 3, area: 28.26, circumference: 18.84});
   });
 });
