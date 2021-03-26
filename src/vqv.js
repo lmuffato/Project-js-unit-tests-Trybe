02e857e14eaef781b6693317c22fdb3848b1e5e7
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-26 18:08:00 -0300

    O Requisito 4 enviado anteriormente na verdade e o requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -27,6 +27,4 @@ const vqv = (nome, idade) => {
   return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
 };
 
-console.log(vqv('Beto', 15));
-
 module.exports = vqv;
