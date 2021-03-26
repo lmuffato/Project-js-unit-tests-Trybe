c5962a671ddab3043f4c6aa73f6a49c1aff52f17
AdelinoJnr <adelinojunior96@gmail.com>
2021-03-26 17:09:06 -0300

    Resoluçao do requisito 1

--- src/average.js
+++ src/average.js
@@ -12,7 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-
+const average = (numbers) => {
+  let soma = 0;
+  if (numbers.length < 1) {
+    return undefined;
+  } else {
+    for (let index = 0; index < numbers.length; index += 1) {
+      if (typeof(numbers[index]) !== 'number') {
+        return undefined;
+      }
+      soma += numbers[index];
+    }
+  }
+  return Math.round(soma / numbers.length);
+};
 module.exports = average;
-//Começar
