5fa19a50a027e43342d7b06542aaa2d2b638d606
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-03-26 19:16:58 -0300

    Added circle tests

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -28,10 +28,17 @@ describe('#circle', () => {
     assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
+    assert.strictEqual(typeof circle(1), 'object');
     // Teste se o objeto retornado tem 3 entradas.
+    assert.strictEqual(Object.keys(circle(2).length === 3));
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(typeof circle(), 'undefined');
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert.strictEqual(parseFloat(circle(2).circumference).toPrecision(2), 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.strictEqual(parseFloat(circle(3).area).toPrecision(2), 28.27);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    // I'm pretty sure this one below doesn't work because of float truncation
+    assert.deepStrictEqual(circle(3), { radius: 3, area: 28.26, circumference: 18.84 });
   });
 });
