3bb0cc7ae57fc22e0f741ba8207279f376838202
Giovanni <giovannitadeu08@gmail.com>
2021-03-26 16:35:53 -0300

    [Requisito 1] - Corrigido.

--- src/average.js
+++ src/average.js
@@ -14,7 +14,7 @@
 
 const average = (array) => {
   if (array.length === 0 || array === undefined) return undefined;
-  for (let index = 0; array.length; index += 1) {
+  for (let index = 0; index < array.length; index += 1) {
     if (typeof array[index] !== 'number') return undefined;
   }
   const med = array.reduce((a, b) => a + b) / array.length;
