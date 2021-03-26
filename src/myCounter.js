36539299d1eb262da0c0a4ceacc3db33ee12d0c7
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-03-26 20:02:32 -0300

    correções eslint

--- src/myCounter.js
+++ src/myCounter.js
@@ -10,20 +10,20 @@
 
   Parâmetros:
   - Nenhum.
-
+gi
   Comportamento:
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
 
-const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
-    }
-  }
-  return myArray;
-};
+// const myCounter = () => {
+//   var myArray = [];
+//   for (var counter = 0; counter <= 3; counter += 1) {
+//     myArray.push(counter);
+//     for (var counter = 2; counter <= 3; counter += 1) {
+//       myArray.push(counter);
+//     }
+//   }
+//   return myArray;
+// };
 
-module.exports = myCounter;
+// module.exports = myCounter;
