d984a9e20482dd6e7fc3de5ca2112be31eb3a943
Flavio Franco <flavio.win@gmail.com>
2021-03-26 16:50:05 -0300

    terminando requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,18 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+
+  if (!nome || !idade) {
+    return undefined;
+  }
+
+  return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+};
+
+console.log(vqv('tunico', '30'));
 
 module.exports = vqv;
