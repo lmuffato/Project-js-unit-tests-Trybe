7f15acc23453b355f17bfef24cf18c54646ba600
Rodrigo Rech <rodrigorech92@gmail.com>
2021-03-26 15:53:31 -0300

    vqv function implemented

--- src/vqv.js
+++ src/vqv.js
@@ -17,13 +17,13 @@
 */
 
 const vqv = (nome, idade) => {
-  if (nome === undefined && idade === undefined) {
+  if (nome === undefined || idade === undefined) {
     return undefined;
   }
   return `Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`
 };
 
 module.exports = vqv;
