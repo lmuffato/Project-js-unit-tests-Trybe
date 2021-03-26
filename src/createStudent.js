afd64c2eeebffbaa124053d75268fabf8cc37eaf
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-03-26 17:26:12 -0300

    :rocket: [Req. 05] - Implemente a função createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  return {
+    name,
+    feedback: () => "Eita pessoa boa!",
+  };
+};
+
+const estudante = createStudent("Leandrão, o Lobo Solitário");
+console.log(estudante);
 
 module.exports = createStudent;
