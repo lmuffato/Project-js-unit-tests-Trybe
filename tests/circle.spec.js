7fafbee264a9b673fb33c143658771b9c32d4f42
Gui-lira <gui.lira.martins@gmail.com>
2021-03-26 16:12:22 -0300

    4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,13 +25,25 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();
+    
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
+    assert.strictEqual(typeof circle(1),'object')
     // Teste se o objeto retornado tem 3 entradas.
+    assert.strictEqual(Object.entries(circle(1)).length, 3)
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(circle(), undefined)
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert.strictEqual(circle(2).circumference, 12.56)
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.strictEqual(parseFloat(circle(3).area.toPrecision(4)), 28.26)
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    const toPrecise = (obj) => {
+      for (let index in obj) {
+        obj[index] = parseFloat(obj[index].toPrecision(4))
+      }
+      return obj
+    }
+    assert.deepStrictEqual(toPrecise(circle(3)), {area: 28.26, circumference: 18.84, radius: 3})
   });
 });
