94a4f7bc3eea6aa1dffd6a4fad86c79562f4c5be
Lucas Pedroso <lepsk8@gmail.com>
2021-03-26 15:27:04 -0300

    productDetails.spec.js done.

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,39 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    //assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails('Gel', 'Máscara')), true);
+
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('Gel', 'Máscara').length, 2)
+
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    const isObjectAllItensInArray = (param) => {
+      const filteredObj = param
+                      .filter((x) => typeof x === 'object' && Array.isArray(x) === false)
+                      .length;
+      if (filteredObj !== param.length) {
+        return false;
+      }
+      return true;
+    };
+
+    assert.strictEqual(isObjectAllItensInArray(productDetails('Gel', 'Máscara')), true);
+
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(productDetails('Gel', 'Máscara')[0] !== productDetails('Gel', 'Máscara')[1], 'true')
+
     // (Difícil) Teste que os dois productIds terminam com 123.
+    const sameProductIdFinal = (product) => {
+      return productDetails('Gel', 'Máscara')
+              .map(x => x.details.productId.substr(-3))
+              .reduce((result, el, index, thisArray) => {
+                if (el === thisArray[0]) return true;
+                return false;
+              });
+    }
+    assert.strictEqual(sameProductIdFinal(productDetails('Gel', 'Máscara')), true);
   });
 });
