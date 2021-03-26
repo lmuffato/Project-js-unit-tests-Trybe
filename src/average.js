cb529183853f8577d93aa6a4dd4a8147fc3d325b
Hernanne <hernannedomiciano@gmail.com>
2021-03-26 16:53:07 -0300

    requisito 1 finalizado

--- src/average.js
+++ src/average.js
@@ -11,7 +11,12 @@
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
-
-const average = () => {};
-
+const average = (array) => {
+  if (array.length === 0 || array === undefined) return undefined;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') return undefined;
+  }
+  const med = array.reduce((a, b) => a + b) / array.length;
+  return Math.round(med);
+};
 module.exports = average;
