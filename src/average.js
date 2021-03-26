9bcd2f5abb4ebe6f306ceae88961dd6918c72288
Edmilson Bernardo <ebedmilson48@gmail.com>
2021-03-26 20:52:19 -0300

    Iniciando o projeto

--- src/average.js
+++ src/average.js
@@ -12,6 +12,24 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+  }
+
+  if (array.length <= 0) {
+    return undefined;
+  }
+
+  let total = array.reduce((acc, num) => {
+    acc += num;
+    return acc;
+  });
+  let result = Math.round(total / array.length);
+
+  return result;
+};
 
 module.exports = average;
