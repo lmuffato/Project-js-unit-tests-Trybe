0aa12fa3bd4616e0a1c2b2d05acf0a5be15dfe74
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-03-26 18:33:10 -0300

    iniciando o projeto

--- src/average.js
+++ src/average.js
@@ -12,6 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (index) => {
+  if (array.length === 0) {
+    return undefined;
+  }
+  let sum = 0;
+  let index;
+  for (index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    sum += array[index];
+  }
+  return Math.round(sum / (index));
+};
 
 module.exports = average;
