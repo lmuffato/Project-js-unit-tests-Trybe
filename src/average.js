b4827e92ee42e90b923a13c65ddf63993429b69f
Lucas Pedroso <lepsk8@gmail.com>
2021-03-26 13:10:25 -0300

    Average.js done.

--- src/average.js
+++ src/average.js
@@ -12,6 +12,25 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+/**
+ * Retorna a média aritmética de um array de números.
+ * @param {array} arrayOnlyNumber 
+ */
+const average = (arrayOnlyNumber) => {
+  let isArrayNumberNotEmpty = true;
+  if (arrayOnlyNumber.length === 0) return undefined;
+  arrayOnlyNumber.forEach((x) => {
+    if(typeof x !== 'number') isArrayNumberNotEmpty = false;
+  });
+  return isArrayNumberNotEmpty 
+    ? arrayOnlyNumber.reduce((result, el, index, thisArray) => {
+                    if (index === thisArray.length - 1) {
+                      result = Math.round(result) + Math.round(el);
+                      return Math.round(result / thisArray.length);
+                    }
+                  return Math.round(result + Math.round(el));
+                }, [])
+    : undefined;
+};
 
 module.exports = average;
