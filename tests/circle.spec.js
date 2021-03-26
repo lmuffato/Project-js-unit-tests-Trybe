b01b41ebc9c8cf98c9e2840064799c0d23d46c2c
Hernanne <hernannedomiciano@gmail.com>
2021-03-26 17:24:44 -0300

    requisito 2

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -27,7 +27,7 @@ describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
     assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se circle retorna um objeto.
+    assert.deepStrictEqual()
     // Teste se o objeto retornado tem 3 entradas.
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
