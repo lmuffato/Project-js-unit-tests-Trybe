a61d4c3744678827a45dbc6bce0986f1eee12b6c
guilhermemd <guilherme.md@gmail.com>
2021-03-26 19:56:29 -0300

    cicle com toPrecision

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -35,8 +35,8 @@ describe('#circle', () => {
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.deepStrictEqual(circle(2).circumference, 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.deepStrictEqual(circle(3).area, 28.259999999999998);
+    assert.deepStrictEqual(circle(3).area.toPrecision(2), 28);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    assert.StrictEqual((circle(3)), { radius: 3, area: 28.259999999999998, circumference: 18.84 });
+    assert.StrictEqual((circle(3)).toPrecision(2), { radius: 3.0, area: 28, circumference: 18 });
   });
 });
