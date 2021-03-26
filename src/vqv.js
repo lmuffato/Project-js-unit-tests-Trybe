6aea8a00c457cfaddd701e9d1196687e3ebeb019
Renzo Sevilha <sevilharenzo@gmail.com>
2021-03-26 15:11:09 -0300

    [REQUISITO 03] lint

--- src/vqv.js
+++ src/vqv.js
@@ -17,7 +17,7 @@
 */
 
 const vqv = (nome, idade) => {
-  if (!nome && !idade) return undefined
+  if (!nome && !idade) return undefined;
   return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
