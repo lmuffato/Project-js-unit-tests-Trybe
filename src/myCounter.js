f02ff15a6ea974fad118172e17ea323b5427acd5
Flavio Franco <flavio.win@gmail.com>
2021-03-26 20:31:41 -0300

    requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -15,15 +15,17 @@
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
 
-/* const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+const myCounter = () => {
+  let myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counter1 = 2; counter1 <= 3; counter1 += 1) {
+      myArray.push(counter1);
     }
   }
   return myArray;
-}; */
+};
 
-/* module.exports = myCounter; */
+console.log(myCounter());
+
+module.exports = myCounter;
