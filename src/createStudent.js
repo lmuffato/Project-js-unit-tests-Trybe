fae498fe4410269220392179e2ffb339bf0f1aa8
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-03-26 18:00:23 -0300

    requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const object = {
+    name: name,
+    feedback: () => 'Eita pessoa boa!'
+  }
+  return object;
+};
 
 module.exports = createStudent;
--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,13 +25,23 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();
+    //assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
+    assert.strictEqual(typeof(circle), 'object');
     // Teste se o objeto retornado tem 3 entradas.
+    assert.strictEqual(Object.entries(circle).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(circle(), 'undefined');
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert.strictEqual(circle(2), 2 * 3.14 * 2);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.strictEqual(circle(3).area, 2 * 3.14 * 3);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert.strictEqual(circle(3), {
+    radius: 3 ,
+    area: 3.14 * 3 * 3,
+    circumference: 2 * 3.14 * 3
+    });
   });
 });
