9b49f7744351d63b89488cbb73ca063f5b6aed64
Eduardo Seije <eduardoseije@gmail.com>
2021-03-26 17:22:51 -0300

    Requisito 4 - adicionando () no cálculo da circ e área.

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -27,15 +27,15 @@ describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
-    assert.strictEqual(typeof(circle(2)), 'object');
+    assert.strictEqual(typeof(circle(1)), 'object');
     // Teste se o objeto retornado tem 3 entradas.
     assert.strictEqual(Object.keys(circle(1)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    assert.strictEqual(Object.values.circle(2)[2], 2 * 3,14 * 2);
+    assert.strictEqual(Object.values.circle(2)[2], (2 * 3,14 * 2));
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.strictEqual(Object.values.circle(3)[1], 3,14 * 3 * 3);
+    assert.strictEqual(Object.values.circle(3)[1], (3,14 * 3 * 3));
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     assert.strictEqual(Object.values.circle(3), [3, (3,14 * 3 * 3), (2 * 3,14 * 3)]);
   });
