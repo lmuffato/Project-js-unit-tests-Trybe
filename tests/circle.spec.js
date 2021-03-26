0d89a6487cd4d76141da1c95ea6114e388c9b296
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-03-26 18:34:18 -0400

    Requisito 4 - Implemente os casos de teste para a função circle.

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,13 +25,20 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
+    assert.strictEqual(typeof circle(1), 'object');
     // Teste se o objeto retornado tem 3 entradas.
+    assert.deepStrictEqual(Object.keys(circle(1)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.deepStrictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert.strictEqual(circle(2).circumference, 12.56); // c = 2·π·r. -> 2.3,14.2 -> c =12,56 //
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.strictEqual(parseFloat(circle(3).area.toFixed(2)), (28.26)); // A = π·r² //
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert.strictEqual(circle(3).radius, 3);
+    assert.strictEqual(parseFloat(circle(3).area.toFixed(2)), 28.26);
+    assert.strictEqual(parseFloat(circle(3).circumference.toFixed(2)), 18.84); // número flutuante: https://stackoverflow.com/questions/1458633/how-to-deal-with-floating-point-number-precision-in-javascript //
   });
 });
