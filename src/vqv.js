77acc5fe13c3e28fbe57a027706a9ec6c944f170
Rafael Ribeiro <rafittu@outlook.com>
2021-03-26 20:54:10 -0300

    alterado indentacao requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -19,8 +19,11 @@
 const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) {
     return undefined;
-  }
-  return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+  }  
+  return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 
 module.exports = vqv;
