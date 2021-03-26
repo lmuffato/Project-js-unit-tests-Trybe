a9ce45e4bf7a2d707ef32fffa6dd28881e33bb21
Guilherme Dugaich de Oliveira <guilherme.dugaich@gmail.com>
2021-03-26 19:53:15 -0300

    corrigindo lint para teste do productDetails spec

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -35,12 +35,12 @@ describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
-    assert.strictEqual(typeof(productDetails()), 'object');
+    assert.strictEqual(typeof (productDetails()), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(productDetails('uno', 'dos').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    assert.strictEqual(typeof(productDetails()[0]), 'object');
-    assert.strictEqual(typeof(productDetails()[1]), 'object');
+    assert.strictEqual(typeof (productDetails()[0]), 'object');
+    assert.strictEqual(typeof (productDetails()[1]), 'object');
     // Teste que os dois objetos são diferentes entre si.
     assert.notStrictEqual(productDetails()[0], productDetails()[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
