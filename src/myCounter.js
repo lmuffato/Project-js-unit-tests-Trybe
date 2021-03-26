cdc045b92016dc32d905e0b8b97a20108a1ca7e3
Giovanni <giovannitadeu08@gmail.com>
2021-03-26 19:29:52 -0300

    Resolvendo problemas do lint v3

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,7 +17,7 @@
 
 const myCounter = () => {
   let myArray = [];
-/*   for (let counter = 0; counter <= 3; counter += 1) {
+  /* for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (let counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
