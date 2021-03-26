1bcdaa8f0d001916fa7ece5d21f641be56e880df
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-03-26 17:48:58 -0300

    Implemente os casos de teste para a função circle, corringindo error

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -29,7 +29,7 @@ describe('#circle', () => {
     assert.strictEqual(Object.entries(circle(2)).length, 3);
     assert.strictEqual(circle(), undefined);
     assert.strictEqual(circle(2).circumference, 12.56)
-    assert.strictEqual(circle(3).area, 28.26);
+    assert.strictEqual(parseFloat((circle(3).area).toPrecision(4)), 28.26);
     assert.deepStrictEqual(circle(3), {radius: 3, area: 28.26, circumference: 18.84});
   });
 });
