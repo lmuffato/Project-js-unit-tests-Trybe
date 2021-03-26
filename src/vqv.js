9ca5268435866303887e65ec6f9cd9ffb98d5c3b
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-03-26 18:09:58 -0300

    Requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -17,10 +17,15 @@
 */
 
 const vqv = (nome, idade) => {
+ 
+  if(nome === undefined || idade === undefined){
+    return undefined;
+  }
+
   return `Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalo na Trybe e mando muito em programação!
-  #VQV!`
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 
 module.exports = vqv;
