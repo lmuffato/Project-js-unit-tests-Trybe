2c11aa773bdd66cb7a34110c8de0800256ec14be
Elisa <elisafranca.carvalho@gmail.com>
2021-03-26 15:37:07 -0300

    Iniciando o projeto: ES6 Testes Unitarios

--- src/average.js
+++ src/average.js
@@ -1,5 +1,5 @@
 /*
-  A função average recebe um array (tamanho variável) e retorna a média dos valores recebidos.
+  A função average recebe um array (tamanho variável) e retorna a média dos valores  recebidos.
   Caso a função receba algum valor não númerico ou um array vazio,
   o valor undefined deve ser retornado.
   Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
