db1f4c574a45faf93e51c27126e0b6fb34ada7ba
Iago de Paula <iago.pferreiravr@gmail.com>
2021-03-26 19:37:44 -0300

    Requisito 7 de novo

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -20,16 +20,16 @@
 */
 
 const calculator = {
-  add: function (num1, num2) {
+  add(num1, num2) {
     return num1 + num2;
   },
-  mult: function (num1, num2) {
+  mult(num1, num2) {
     return (num1 * num2);
   },
-  div: function (num1, num2) {
+  div(num1, num2) {
     return Math.floor(num1 / num2);
   },
-  sub: function (num1, num2) {
+  sub(num1, num2) {
     return (num1 - num2);
   }
 };
