552f24d7aa882f374d2630bef90ea13dccf897eb
madruga665 <madruga665@hotmail.com>
2021-03-26 19:54:25 -0300

    lint me ajuda

--- src/average.js
+++ src/average.js
@@ -14,15 +14,15 @@
 
 const average = (array) => {
   if (array.length === 0) return undefined;
-  
+
   for (let index = 0; index < array.length; index += 1) {
     if (typeof (array[index]) !== 'number') {
       return undefined;
     }
   }
   let sum = array.reduce((accumulator, index) => accumulator + index, 0);
-  let average = sum / array.length;
-  return Math.round(average);
+  let result = sum / array.length;
+  return Math.round(result);
 };
 
 console.log(average([]));
