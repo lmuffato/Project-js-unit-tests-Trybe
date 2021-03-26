b6a49dc04eaa13e7d9e666d48de73a0db52c8140
Felipe Flores <felipefloresvendas@gmail.com>
2021-03-26 16:56:02 -0300

    Requirement 5 Done

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  let output = {
+    name,
+    feedback: feedback = () => 'Eita pessoa boa!',
+  }
+  return output;
+};
 
 module.exports = createStudent;
