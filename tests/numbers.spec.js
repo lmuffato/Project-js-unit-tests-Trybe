2b1e237f4a8bf752215a201b52f2665bfdbf8f71
guilhermemd <guilherme.md@gmail.com>
2021-03-26 19:35:12 -0300

    mais um teste lint

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -6,13 +6,11 @@ const numbers = require('../src/numbers');
 
 /*
   A função numbers recebe um array (tamanho variável) e retorna true se todos os parâmetros forem do tipo 'number' e false caso contrário.
-
   Parâmetros:
     - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, 'a']; [].
   Comportamento:
     - numbers([2, 3, 4]); // Retorna: true
     - numbers([2, 'errado', 5]); // Retorna: false
-
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
