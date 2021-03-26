d162f601dcc560367ac4dc5dce9540bc885c64c8
Patrick Morais <ppternunes@gmail.com>
2021-03-26 16:13:01 -0300

    fix

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -39,3 +39,4 @@ describe('#circle', () => {
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     assert.deepStrictEqual(circle(3), { radius: 3, area: 28.26, circumference: 18.84 });
 });
+});
