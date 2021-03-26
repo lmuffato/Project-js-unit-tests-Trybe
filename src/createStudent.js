39a59a9d3c60d2ab33d5fa3c066253ef7fc0e06f
Edrei <edreimateus@gmail.com>
2021-03-26 18:27:18 -0300

    Adiciona requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const student = {};
+  student.name = nome;
+  student.feedback = () => 'Eita pessoa boa!';
+  return student;
+};
 
 module.exports = createStudent;
