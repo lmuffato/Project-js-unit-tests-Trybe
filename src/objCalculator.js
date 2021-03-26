2a596111dd57d6a74f15a8a63ff459e2b1be58fa
Renzo Sevilha <sevilharenzo@gmail.com>
2021-03-26 15:27:22 -0300

    [REQUISITO 07]

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,11 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  add: (num, num2) => num + num2,
+  mult: (num, num2) => num * num2,
+  div: (num, num2) => Math.floor(num / num2),
+  sub: (num, num2) => num - num2,
+};
 
 module.exports = calculator;
