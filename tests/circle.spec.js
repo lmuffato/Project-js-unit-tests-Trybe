2da2ee96fa1f60a269d224b4729659578271e00e
William Willers <w.willers@outlook.com>
2021-03-26 18:16:11 -0400

    Quebra de linha Circle wtf

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -37,6 +37,8 @@ describe('#circle', () => {
     assert.strictEqual(circle(), undefined);
     assert.strictEqual(circle(2).circumference, 12.56);
     assert.strictEqual(circle(3).area, 28.259999999999998);
-    assert.deepStrictEqual(circle(3), { radius: 3, area: 28.259999999999998, circumference: 18.84 });
+    assert.deepStrictEqual(circle(3), { radius: 3,
+      area: 28.259999999999998,
+      circumference: 18.84 });
   });
 });
