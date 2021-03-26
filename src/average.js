0575901ad96841923442c494f3c8c9de991717b1
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-03-26 15:43:56 -0300

    Implemente a função average done

--- src/average.js
+++ src/average.js
@@ -12,6 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = arr => {
+  let total = 0;
+  let len = arr.length;
+  let media;
+  if (arr.length === 0) return undefined;
+  for (let index = 0; index < arr.length; index += 1) {
+    if (typeof arr[index] !== 'number') return undefined;
+    total += arr[index];
+  }
+
+  media = total/len;
+  return Math.round(media);
+};
 
 module.exports = average;
