905ab753f7b2a47b91d745de20bce7a115d246dc
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-03-26 19:25:56 -0300

    correção eslint requisito 1

--- src/average.js
+++ src/average.js
@@ -13,19 +13,16 @@
 */
 
 const average = (array) => {
-
-  if (typeof(array) !== 'object' || array.length === 0) {
+  if (typeof (array) !== 'object' || array.length === 0) {
     return undefined;
   }
 
   let soma = 0;
   let media = 0;
   for (let index = 0; index < array.length; index += 1) {
-
-    if (typeof(array[index]) === 'string') {
+    if (typeof (array[index]) === 'string') {
       return undefined;
     }
-
     soma += array[index];
     media = Math.round(soma / array.length);
   }
