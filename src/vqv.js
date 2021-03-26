c89b01d29e8b12de3ba3fde709199288c4138471
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-03-26 18:14:17 -0300

    Requisito 3: Correçao formataçao da resposta

--- src/vqv.js
+++ src/vqv.js
@@ -17,15 +17,12 @@
 */
 
 const vqv = (nome, idade) => {
- 
   if(nome === undefined || idade === undefined){
     return undefined;
   }
-
   return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
 };
-
 module.exports = vqv;
