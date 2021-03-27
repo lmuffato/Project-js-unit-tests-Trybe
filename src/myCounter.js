a6745e6c4c39e5fdaeb5e257d0f6fa3bf0582076
Hernanne <hernannedomiciano@gmail.com>
2021-03-26 21:40:30 -0300

    teste 9

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,12 +17,13 @@
 
 const myCounter = () => {
   const myArray = [];
-  for (let counter = 0; counter <= 3; counter += 1) {
+  /*for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (let counter1 = 2; counter1 <= 3; counter1 += 1) {
       myArray.push(counter1);
     }
   }
+  */
   return myArray;
 };
 
