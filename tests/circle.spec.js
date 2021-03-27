1afce6446df8bd517705db941b9db5cace425e8a
Alexandre Oliveira <alexandremolive@gmail.com>
2021-03-27 20:47:20 -0300

    exercicio 5

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -31,11 +31,5 @@ describe('#circle', () => {
     assert.strictEqual(circle(), undefined);
     assert.strictEqual(circle(2).circumference, 12.56);
     assert.strictEqual(parseFloat(circle(3).area.toPrecision(2)), 28);
-    // Teste se circle retorna um objeto.
-    // Teste se o objeto retornado tem 3 entradas.
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
   });
 });
