65eb52e71ba1487c61b2f5c04095bd476ba70ed2
Hernanne <hernannedomiciano@gmail.com>
2021-03-26 22:13:38 -0300

    REQUISITO 3 FINALMENTE

--- src/vqv.js
+++ src/vqv.js
@@ -21,6 +21,6 @@ const vqv = (nome, idade) => {
   return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+#VQV!`;
 };
 module.exports = vqv;
