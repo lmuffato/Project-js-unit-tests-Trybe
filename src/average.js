593163774a6236c06ca39d33d609e3bf3732cd04
rodrigoaqueiroz <rodrigoandradequeiroz@gmail.com>
2021-03-27 09:49:44 -0300

    começando o projeto. VQV!

--- src/average.js
+++ src/average.js
@@ -1,17 +1,21 @@
-/*
-  A função average recebe um array (tamanho variável) e retorna a média dos valores recebidos.
-  Caso a função receba algum valor não númerico ou um array vazio,
-  o valor undefined deve ser retornado.
-  Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
+const average = (arrayNumbers) => {
+  let result = 0;
+  if (arrayNumbers.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < arrayNumbers.length; index += 1) {
+    if (typeof (arrayNumbers[index]) !== 'number') {
+      return undefined;
+    }
+    result += Math.round((arrayNumbers[index])) / arrayNumbers.length;
+  }
+  return result;
+};
 
-  Parâmetros:
-    - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
-  Comportamento:
-    - average([2, 2]) // Retorno: 2;
-    - average([1, 1]) // Retorno: 1;
-    - average([1, '2']) // Retorno: undefined;
-*/
-
-const average = () => {};
+console.log(average([2, 2]));
+console.log(average([1, '2']));
+console.log(average([1, 5, 8]));
+console.log(average([1, '']));
+console.log(average([]));
 
 module.exports = average;
