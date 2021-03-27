170c943ed18c12f1a972f897ba9212d87ab27a29
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-03-27 11:05:28 -0300

    Correcao de erros do ESlint

--- src/vqv.js
+++ src/vqv.js
@@ -20,8 +20,7 @@ const vqv = (nome, idade) => {
   if (nome === undefined && idade === undefined) {
     return undefined;
   }
-  return `Oi, meu nome é${nome}!\n
-  Tenho${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+  return `Oi, meu nome é${nome}!\nTenho${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
 };
 
 module.exports = vqv;
