3020ae90429a63f513dabe3aa79d5678605b36ef
Guilherme Sucupira Oliveira <guilherme.so23@gmail.com>
2021-03-26 22:31:06 -0300

    Iniciando projeto js-unit-tests

--- src/average.js
+++ src/average.js
@@ -12,6 +12,15 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (numbers) => {
+  let sum = 0;
+  for (let index = 0; index < numbers.length; index += 1) {
+    if (typeof numbers[index] !== 'number') {
+      return undefined;
+    }
+    sum += numbers[index];
+  }
+  return numbers.length === 0 ? undefined : (Math.round(sum / numbers.length));
+};
 
 module.exports = average;
