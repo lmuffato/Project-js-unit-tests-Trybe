9ab4d224040a2ae6604094edfc63d818f91f1737
mallmann02 <leomallmann020302@gmail.com>
2021-03-27 15:30:08 -0300

    Refatora função

--- src/createStudent.js
+++ src/createStudent.js
@@ -16,10 +16,11 @@
 */
 
 const createStudent = (name) => {
-  return {
-    name: name,
+  const estudante = {
+    name,
     feedback: () => 'Eita pessoa boa!',
-  }
+  };
+  return estudante;
 };
 
 module.exports = createStudent;
