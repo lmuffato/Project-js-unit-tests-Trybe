4f1dd429699c34b444a8286b884551220bff7b57
Willian Mendes <willianmendesbr@gmail.com>
2021-03-26 22:29:23 -0300

    Requisito 4 - Fix 3

--- src/circle.js
+++ src/circle.js
@@ -23,7 +23,7 @@ const circle = (radius) => {
   if (!radius) { return undefined; }
   return {
     radius,
-    area: parseFloat((PI * (radius * radius)).toFixed(2)),
+    area: +(PI * radius * radius).toFixed(2),
     circumference: 2 * PI * radius,
   };
 };
