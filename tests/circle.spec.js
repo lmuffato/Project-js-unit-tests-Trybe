276a676ecad5dc3dcb535a41bc0788d926757c48
guilhermemd <guilherme.md@gmail.com>
2021-03-27 18:11:26 -0300

    requisito 4 entendendo pq preciso do float

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -30,10 +30,10 @@ describe('#circle', () => {
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.strictEqual(circle(2).circumference, 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.deepStrictEqual(circle(3).area.toPrecision(2), 28);
+    assert.deepStrictEqual(parseFloat(circle(3).area.toPrecision(2)), 28);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     assert.deepStrictEqual(circle(3).radius, 3);
-    assert.deepStrictEqual(circle(3).area.toPrecision(2), 28);
-    assert.deepStrictEqual(circle(3).circumference.toPrecision(2), 19);
+    assert.deepStrictEqual(parseFloat(circle(3).area.toPrecision(2)), 28);
+    assert.deepStrictEqual(parseFloat(circle(3).circumference.toPrecision(2)), 19);
   });
 });
