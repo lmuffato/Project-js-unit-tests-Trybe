92b79a3993514745233635ea9d7423f3e492a682
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-03-27 20:48:08 -0300

    requesito 1

--- src/average.js
+++ src/average.js
@@ -11,9 +11,34 @@
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
-
-const average = () => {
-// add your implementation here!!
+let testArray = [];
+const checkIfNumber = (array) => {
+  let verificatorN = 0;
+  let checkNum = true;
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      verificatorN += 1;
+    }
+  }
+  if (verificatorN > 0) {
+    checkNum = false;
+  }
+  if (array.length === 0) {
+    checkNum = false;
+  }
+  return checkNum;
+};
+const average = (array) => {
+  const condition = checkIfNumber(array);
+  let avarageResult = 0;
+  if (condition) {
+    for (let i = 0; i < array.length; i += 1) {
+      avarageResult += array[i];
+    }
+  } else {
+    avarageResult = undefined;
+  }
+  return avarageResult;
 };
 
 module.exports = average;
