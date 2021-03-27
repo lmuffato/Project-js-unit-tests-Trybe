039cedd1f6da28161b5977be855c509ba748d1b7
Guilherme Sucupira Oliveira <guilherme.so23@gmail.com>
2021-03-26 23:20:43 -0300

    Concluido exercicio Circle

--- src/circle.js
+++ src/circle.js
@@ -20,7 +20,7 @@
 
 const circle = (radius) => {
   const PI = 3.14;
-  if (!radius) { return undefined; }
+  if (!radius || typeof radius !== 'number') { return undefined; }
   return {
     radius,
     area: PI * radius * radius,
