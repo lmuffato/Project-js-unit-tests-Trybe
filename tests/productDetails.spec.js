87ac9fea38a9615d861fe116288ffd5481f1d624
rodrigoaqueiroz <rodrigoandradequeiroz@gmail.com>
2021-03-27 13:13:28 -0300

    sexto requisito

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,17 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste que o retorno da função é um array.
-    // Teste que o array retornado pela função contém dois itens dentro.
-    // Teste que os dois itens dentro do array retornado pela função são objetos.
-    // Teste que os dois objetos são diferentes entre si.
-    // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(Array.isArray(productDetails), true);
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara').length, 2);
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara')[0], 'object');
+    assert.strictEqual(productDetails('Luva', 'Água')[1], 'object');
+    assert.strictEqual(productDetails('Agua sanitária', 'Máscara')[0]
+      !== productDetails('Sabonete', 'Máscara')[1], false);
+    assert.strictEqual(productDetails('Desinfetante', 'Máscara')[0]
+      .details.productId
+      .slice(-3), '123');
+    assert.strictEqual(productDetails('Detergente', 'Máscara')[1]
+      .details.productId
+      .slice(-3), '123');
   });
 });
