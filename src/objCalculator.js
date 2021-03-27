c477d3c72ae0426386589ddd8075a33ad524fe20
mallmann02 <leomallmann020302@gmail.com>
2021-03-27 15:40:30 -0300

    Insere vírgula no final da linha 26

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -23,7 +23,7 @@ const calculator = {
   add: (x, y) => x + y,
   mult: (x, y) => x * y,
   div: (x, y) => Math.floor(x / y),
-  sub: (x, y) => x - y
+  sub: (x, y) => x - y,
 };
 
 module.exports = calculator;
