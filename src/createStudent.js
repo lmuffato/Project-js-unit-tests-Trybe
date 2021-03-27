26f2f724cd8402ec04ea6cadc0cfd43a6c12fdbc
rodrigoaqueiroz <rodrigoandradequeiroz@gmail.com>
2021-03-27 12:43:11 -0300

    quinto requisito

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const feedback = () => 'Eita pessoa boa!';
+  const student = {
+    name,
+    feedback,
+  };
+  return student;
+};
 
 module.exports = createStudent;
