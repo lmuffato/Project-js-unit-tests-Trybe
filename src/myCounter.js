9ea4ced0ea6b625417352b17ff932026e5ed9951
Edrei <edreimateus@gmail.com>
2021-03-27 12:13:02 -0300

    Adiciona requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,9 +17,9 @@
 
 const myCounter = () => {
   let myArray = [];
-  for (let counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (let counterr = 2; counter <= 3; counter += 1) {
+  for (let index = 0; index <= 3; index += 1) {
+    myArray.push(index);
+    for (let counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
