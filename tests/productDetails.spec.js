7a2ba357cc66b6b7b555a5521d464082a886c9a9
Eduardo Seije <eduardoseije@gmail.com>
2021-03-27 18:21:00 -0300

    Requisito 07 - teste 1

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,25 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    const arrayProduct = Object.entries(productDetails('a', 'b'));
+    assert.strictEqual(Array.isArray(arrayProduct), true); // source https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('a','b').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    const checkObjType = (obj) => {
+    for (let key in arrayProduct) {
+      if (typeof arrayProduct[key] === 'object');
+      return true;
+      }
+      return false;
+    }
+    assert.strictEqual(checkObjType(arrayProduct), true);
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(productDetails('a', 'b')[0], productDetails('a', 'b')[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+
+
   });
 });
