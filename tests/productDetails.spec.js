52822024231b435b885b60f05c2e540719ed9108
Hernanne <hernannedomiciano@gmail.com>
2021-03-27 19:50:43 -0300

    ajustes lint 2

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -38,14 +38,14 @@ describe('#productDetails', () => {
     for (let index = 0; index < 2; index += 1) {
       assert.deepStrictEqual(typeof productDetails('alcool', 'gel')[index], 'object');
     }
-    assert.deepStrictEqual(productDetails('gel', 'mascara')[0] !==
-      productDetails('gel', 'mascara')[1], true);
+    assert.deepStrictEqual(productDetails('gel', 'mascara')[0]
+    !==  productDetails('gel', 'mascara')[1], true);
     for (let index = 0; index < 2; index += 1) {
       const teste = productDetails('1', '2')[index].details.productId.split('');
       const teste2 = teste.length;
       assert.strictEqual(teste[teste2 - 3], '1');
       assert.strictEqual(teste[teste2 - 2], '2');
       assert.strictEqual(teste[teste2 - 1], '3'); // Feito com base e ajuda do Bruno Mendes
-    };
+    }
   });
 });
