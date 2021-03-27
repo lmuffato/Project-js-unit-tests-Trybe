a1cc7747fb8911a0d938f2f73d1f1a9069c2800c
Giovanni <giovannitadeu08@gmail.com>
2021-03-26 21:02:20 -0300

    Resolvendo problemas do lint v4

--- src/circle.js
+++ src/circle.js
@@ -14,7 +14,7 @@
     - circle(3) // Retorno: {radius: 3, area: 28,26, circumference: 18.84}
 
   DICA: Números de ponto flutuante em JavaScript são imprecisos! Para testar, vá no seu navegador e faça `0.2 + 0.1`.
-        Uma solução pra isso pode ser fazer a soma no seguinte formato: `parseFloat((0.2 + 0.1).toPrecision(2))`.
+        Uma solução pra isso pode ser fazer a soma no seguinte formato: `parseFloat((0.2 + 0.1).toPrecision(2))`
         Use esse conhecimento para te ajudar a lidar com possíveis problemas que esses testes trarão!
 */
 
