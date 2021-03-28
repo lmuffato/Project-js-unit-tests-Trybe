9a615cc4a3104d3dc375a199db6a8a2231b166ed
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-03-28 00:08:51 -0300

    exe 7

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,15 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  add: (num1, num2) => Math.floor(num1 + num2),
+  mult: (num1, num2) => Math.floor(num1 * num2),
+  div: (num1, num2) => Math.floor(num1 / num2),
+  sub: (num1, num2) => Math.floor(num1 - num2),
+};
 
 module.exports = calculator;
+
+// Referencias:
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/floor
+// https://github.com/tryber/sd-010-a-project-js-unit-tests/pull/28/files
