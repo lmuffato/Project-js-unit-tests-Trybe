44e721cee0cd48c381e9ec9f068f446b601b1ef9
igson <igson660@gmail.com>
2021-03-28 11:06:06 -0500

    consert quesito 08

--- src/myCounter.js
+++ src/myCounter.js
@@ -14,8 +14,8 @@ const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counte = 2; counte <= 3; counte += 1) {
+      myArray.push(counte);
     }
   }
   return myArray;
