28e109d33ea90e411e4022d4895339e99bd4662e
carlos25xrm <carlos25xrm@gmail.com>
2021-03-28 17:41:20 -0300

    Refazendo(3) o Requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -24,10 +24,11 @@ const vqv = (nome, idade) => {
   if (!nome || !idade) {
     return undefined;
   }
-  return `Oi, meu nome é ${nome}!
+  const paragrafo = `Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
   trabalho na Trybe e mando muito em programação!
   #VQV!`;
+  return paragrafo;
 };
 
 module.exports = vqv;
