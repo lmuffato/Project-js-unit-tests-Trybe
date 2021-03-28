9866447cd8b286bc886640cb7c63f641176a19b2
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-03-28 09:29:17 -0300

    exe 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -11,16 +11,14 @@
 */
 
 const myCounter = () => {
-  const myArray = [1];
-  for (let counter = 0; counter < myArray.length; counter += 1) {
+  const myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (counter = 2; counter < myArray.length; counter += 1) {
-      myArray.push(counter);
+    for (let i = 2; i <= 3; i += 1) {
+      myArray.push(i);
     }
   }
   return myArray;
 };
-
-console.log(myCounter());
 
 module.exports = myCounter;
