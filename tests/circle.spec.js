1366f95707c63d7fd95215d2e31f2c21461825dc
igson <igson660@gmail.com>
2021-03-27 21:55:07 -0500

    conserta quesito 04

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -29,7 +29,7 @@ describe('#circle', () => {
     // Teste se circle retorna um objeto.
     assert.strictEqual(typeof circle(1), 'object');
     // Teste se o objeto retornado tem 3 entradas.
-    assert.deepstrictEqual(object.entries(circle(1)).length, 3);
+    assert.deepStrictEqual(object.entries(circle(1)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.deepstrictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
