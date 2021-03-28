43ffc16d4d5fe5148d03193418f02a753daa69c3
Eric <ericff07@gmail.com>
2021-03-28 15:27:38 -0300

    Realiza o requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,16 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (nome === undefined) {
+    return undefined;
+  } {
+    let presentation = (`Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`);
+    return presentation;
+  }
+};
 
 module.exports = vqv;
