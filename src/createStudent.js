fd0dcf587b3964d9fcb56905f6a7509397a3ea13
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-03-28 16:57:24 -0300

    requisito 5 new linter fix"

--- src/createStudent.js
+++ src/createStudent.js
@@ -20,7 +20,7 @@ const createStudent = (str) => {
     name: str,
     feedback: 'Eita pessoa boa!',
   };
-  return estudante
+  return estudante;
 };
-console.log(createStudent('Leandrão, o Lobo Solitário'))
+
 module.exports = createStudent;
