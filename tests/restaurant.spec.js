e43a3a73f6be94248b7cd4e9497c3ec1dae3f117
mallmann02 <leomallmann020302@gmail.com>
2021-03-28 16:15:05 -0300

    Teste 5

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -87,6 +87,8 @@ describe('#createMenu', () => {
     // TESTE 5: Verifique que chamar uma função associada à chave `order` no objeto retornado, passando uma string como parâmetro,
     // como `objetoRetornado.order('coxinha')`, tal string é adicionada ao array retornado em `objetoRetornado.consumption
     // ```
+    objetoRetornado.order("coxinha");
+    assert.strictEqual(objetoRetornado.consumption, ["coxinha"]);
     // const objetoRetornado = createMenu(objetoQualquer);
     // objetoRetornado.order("coxinha");
     // objetoRetornado.consumption // Retorno: ["coxinha"]
