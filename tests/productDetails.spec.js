8063917c03f91826e42efb71eebc93de22b62a15
Bruno Mendes <mendessbruno@gmail.com>
2021-03-28 16:20:33 -0300

    rtesolvido problema exercicio 6. no exercicio 9 tive que fazer agumas alteraçoes do proposto, para funcionar direito os testes

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -34,7 +34,7 @@ const productDetails = require('../src/productDetails');
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
     // ESCREVA SEUS TESTES ABAIXO:
-    assert.strictEqual(Array.isArray(productDetails('1', '2')), 'true');
+    assert.strictEqual(Array.isArray(productDetails('1', '2')), true);
     assert.strictEqual(productDetails('1', '2').length, 2);
     for (let index = 0; index < 2; index += 1) {
       assert.strictEqual(typeof productDetails('1', '2')[index], 'object');
