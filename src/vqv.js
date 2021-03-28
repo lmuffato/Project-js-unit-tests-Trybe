a85d3e3496c8932851d3800dd95ea069e0de82dc
Vinicios Fraga <soicinivt@gmail.com>
2021-03-27 23:21:01 -0300

    ajuste lint.

--- src/vqv.js
+++ src/vqv.js
@@ -20,7 +20,8 @@ const vqv = (nome, idade) => {
   if (!nome || !idade) {
     return undefined;
   }
-  return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`
+  return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
 };
 
 module.exports = vqv;
