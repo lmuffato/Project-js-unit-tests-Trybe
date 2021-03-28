b32edae3b7534d0bb16b5b030478d9e39aeee28f
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-03-28 00:16:20 -0300

    Remove comentários desnecessários

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -4,47 +4,12 @@
 const assert = require('assert');
 const productDetails = require('../src/productDetails');
 
-/*
-  Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
-
-  Parâmetros:
-  - Uma string;
-  - Uma string;
-
-  Comportamento:
-  productDetails('Alcool gel', 'Máscara') // Retorna:
-  [
-    {
-      name: 'Alcool gel'
-      details: {
-        productId: 'Alcool gel123'
-      }
-    },
-    {
-      name: 'Máscara'
-      details: {
-        productId: 'Máscara123'
-      }
-    }
-  ]
-
-  OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
-*/
-
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste que o retorno da função é um array.
-    const teste = productDetails('Alcool gel', 'Máscara');
-    const teste2 = [{ name: undefined,
-      details: { productId: 'undefined123' } },
-    { name: undefined, details: { productId: 'undefined123' } }];
-    assert.strictEqual(Object.prototype.toString.call(productDetails(teste)), '[object Array]');
-    // Teste que o array retornado pela função contém dois itens dentro.
+    assert.ok(Object.prototype.toString.call(productDetails()) === '[object Array]');
     assert.strictEqual(productDetails('Alcool gel', 'Máscara').length, 2);
-    // Teste que os dois itens dentro do array retornado pela função são objetos.
-    assert.deepStrictEqual(productDetails(), teste2);
-    // Teste que os dois objetos são diferentes entre si.
-    // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.ok(Object.prototype.toString.call(productDetails()[0]) === '[object Object]' && Object.prototype.toString.call(productDetails()[1]) === '[object Object]');
+    assert.notDeepStrictEqual(productDetails('Álcool gel', 'Máscara')[0], productDetails('Álcool Gel', 'Máscara')[1]);
+    assert.ok(productDetails()[0].details.productId === 'undefined123' && productDetails()[1].details.productId === 'undefined123');
   });
 });
