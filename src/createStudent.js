7beab4a4e1739a1d9bb8a192a28a491e75b12d38
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-03-29 18:05:30 -0300

    requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  let student = {
+    name,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return student;
+};
 
 module.exports = createStudent;
