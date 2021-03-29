4019d65f5b37d249778f3d3cfebb7509cfcfe4b5
Lucas Muffato <lmiremuffato@gmail.com>
2021-03-29 10:35:08 -0300

    requisito product details

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -5,30 +5,33 @@ const assert = require('assert');
 
 const productDetails = require('../src/productDetails');
 
+const productSample1 = 'Alcool gel';
+const productSample2 = 'Máscara';
+
 function productIdMustEndWith123(array) {
   let condiction = '';
   for (let key in array) {
-    if (productDetails('Alcool gel', 'Máscara')[key].details.productId.endsWith('123') === false)
-    { condiction = false; break; }
-    if (productDetails('Alcool gel', 'Máscara')[key].details.productId.endsWith('123') === true)
-    { condiction = true; }
+    if (productDetails(productSample1, productSample2)[key].details.productId.endsWith('123') === false) {
+    condiction = false; break; }
+    if (productDetails(productSample1, productSample2)[key].details.productId.endsWith('123') === true) {
+    condiction = true; }
   } return condiction;
 }
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
     // Teste que o retorno da função é um array.
-    assert.strictEqual(Array.isArray(productDetails('Alcool gel', 'Máscara')), true); // Retorna verdadeiro se o resultado é um array
+    assert.strictEqual(Array.isArray(productDetails(productSample1, productSample2)), true); // Retorna verdadeiro se o resultado é um array
     // Teste que o array retornado pela função contém dois itens dentro.
-    assert.strictEqual(productDetails('Alcool gel', 'Máscara').length, 2);
+    assert.strictEqual(productDetails(productSample1, productSample2).length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    assert.strictEqual(typeof productDetails('Alcool gel', 'Máscara')[0] === 'object', true);
-    assert.strictEqual(typeof productDetails('Alcool gel', 'Máscara')[1] === 'object', true);
+    assert.strictEqual(typeof productDetails(productSample1, productSample2)[0] === 'object', true);
+    assert.strictEqual(typeof productDetails(productSample1, productSample2)[1] === 'object', true);
     // Teste que os dois objetos são diferentes entre si.
     assert.notStrictEqual(
-      productDetails('Alcool gel', 'Máscara')[0], productDetails('Alcool gel', 'Máscara')[0]
+      productDetails(productSample1, productSample2)[0], productDetails(productSample1, productSample2)[0],''
     );
     // (Difícil) Teste que os dois productIds terminam com 123.
-    assert.strictEqual(productIdMustEndWith123(productDetails('Alcool gel', 'Máscara')), true);
+    assert.strictEqual(productIdMustEndWith123(productDetails(productSample1, productSample2)), true);
   });
 });
