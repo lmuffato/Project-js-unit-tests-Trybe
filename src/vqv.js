e5e021b4126a576e6f544e1cf8dd987f2dd6d8d9
Marcelo Maurício <mmauricio.design@gmail.com>
2021-03-29 11:14:46 -0300

    Requisito 2 - criado a funcao que recebe nome e idade, e retorna uma mensagem

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,17 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
+
+  const messageVqv = `'Oi, meu nome é ${nome},
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!,
+#VQV!'`;
+
+  return messageVqv;
+};
 
 module.exports = vqv;
