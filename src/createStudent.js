d1315324f902a02afd6972e899c90be7d8330afb
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-03-28 21:23:23 -0300

    Requisito createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (estudante) => {
+  const estudanteNomeFrase = {
+    name: estudante,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return estudanteNomeFrase;
+};
 
 module.exports = createStudent;
