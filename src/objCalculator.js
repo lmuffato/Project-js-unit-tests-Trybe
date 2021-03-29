cb753fbba92f8c0ea6ecea5164afd6d033315310
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-03-29 12:23:35 -0300

    Sorry lint, fixed

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -18,32 +18,31 @@
   calculator.add(1, 1) // Retorno: 2;
   calculator.div(3, 2) // Retorno: 1;
 */
-
+const TYPE_NOT_INTEGER = 'Please use integers';
 const sum = (a, b) => {
   if (!Number.isInteger(a) || !Number.isInteger(b)) {
-    throw TypeError('Please use integers');
+    throw TypeError(TYPE_NOT_INTEGER);
   }
   return a + b;
 };
 const subtract = (a, b) => {
   if (!Number.isInteger(a) || !Number.isInteger(b)) {
-    throw TypeError('Please use integers');
+    throw TypeError(TYPE_NOT_INTEGER);
   }
   return a - b;
 };
 const multiply = (a, b) => {
   if (!Number.isInteger(a) || !Number.isInteger(b)) {
-    throw TypeError('Please use integers');
+    throw TypeError(TYPE_NOT_INTEGER);
   }
   return a * b;
 };
 const divide = (a, b) => {
   if (!Number.isInteger(a) || !Number.isInteger(b)) {
-    throw TypeError('Please use integers');
+    throw TypeError(TYPE_NOT_INTEGER);
   }
   return Math.floor(a / b);
 };
-
 
 const calculator = {
   add: sum,
