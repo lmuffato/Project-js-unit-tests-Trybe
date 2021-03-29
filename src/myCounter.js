38024ec8eafe86404fe3d639185637af2b646966
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-03-29 12:38:03 -0300

    Fixing myCounter function logic

--- src/myCounter.js
+++ src/myCounter.js
@@ -9,14 +9,11 @@
   Comportamento:
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
-
+const addElemsToArray = (elem, array) => array.push(elem, 2, 3);
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
-    }
+  const myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
+    addElemsToArray(counter, myArray);
   }
   return myArray;
 };
