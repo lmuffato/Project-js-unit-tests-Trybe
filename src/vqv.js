7f61e31599c4c13137945731d271742475a07d1d
lucas-paz <lucas.hpaz@outlook.com>
2021-03-29 15:37:08 -0300

    Corrige função vqv: adiciona retorno a string

--- src/vqv.js
+++ src/vqv.js
@@ -19,12 +19,11 @@
 const vqv = (nome, idade) => {
   if (!nome || !idade) {
     return undefined;
-  } else {
-    `Oi, meu nome é ${nome}!
-    Tenho ${idade} anos,
-    trabalho na Trybe e mando muito em programação!
-    #VQV!`
   }
+  return `Oi, meu nome é ${nome}!
+  Tenho ${idade} anos,
+  trabalho na Trybe e mando muito em programação!
+  #VQV!`
 };
 
 module.exports = vqv;
