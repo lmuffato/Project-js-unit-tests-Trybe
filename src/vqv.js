b5271c6c30ceedcb6494cdf0c5466e1e0bc9e500
Marcelo Maurício <mmauricio.design@gmail.com>
2021-03-29 11:29:50 -0300

    Requisito 2 - acerto da mensagem para teste do evaluator - 2

--- src/vqv.js
+++ src/vqv.js
@@ -21,7 +21,7 @@ const vqv = (nome, idade) => {
     return undefined;
   }
 
-  const messageVqv = `Oi, meu nome é ${nome},
+  const messageVqv = `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
