ec08b85bb6ee2ff3c563b95283e451daf77ac536
lucas-chave <lcchavesr150@gmail.com>
2021-03-29 20:19:34 -0300

    corrigindo lint

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -36,7 +36,7 @@ describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
-    assert.strictEqual(Array.isArray(productDetails('Alcohol', 'Mask')) ? 'array' : null, 'array');
+    assert.strictEqual(Array.isArray(productDetails('Alcohol', 'Mask')) ? 'array' : NaN, 'array');
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(productDetails('Alcohol', 'Mask').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
@@ -44,6 +44,7 @@ describe('#productDetails', () => {
     // Teste que os dois objetos são diferentes entre si.
     assert.notStrictEqual(productDetails('Alcohol', 'Mask')[0], productDetails('Alcohol', 'Mask')[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
-    assert.strictEqual(productDetails('Alcohol', 'Mask')[0].details.productId.substr(-3), productDetails('Alcohol', 'Mask')[1].details.productId.substr(-3));
+    assert.strictEqual(productDetails('Alcohol', 'Mask')[0].details.productId.substr(-3),
+      productDetails('Alcohol', 'Mask')[1].details.productId.substr(-3));
   });
 });
