34cc78821556f4348051e1b34b4f554ffca970c2
AdelinoJnr <adelinojunior96@gmail.com>
2021-03-29 19:03:50 -0300

    Correçao requisito 9 (Eslint meu amor, funciona)

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,32 +1,21 @@
 /* eslint-disable max-len */
 
-/*
-  Você é responsável por escrever o código do sistema de pedidos de um restaurante. Deve ser possível, através desse sistema,
-  cadastrar um menu. Dado que um menu foi cadastrado, o sistema deve disponibilizar um objeto através do qual se consegue:
-  - ler o menu cadastrado;
-  - fazer pedidos;
-  - verificar o que foi pedido;
-  - somar o valor da conta.
-*/
+function generatorPay(foods, drinks, consun) {
+  let pay = 0;
+  let quantidadeDeItemCardapio = 2;
 
-// const generatorPay = (foods, drinks, consun) => {
-//   console.log('to na função');
-//   let pay = 0;
-
-//   consun.forEach((item) => {
-//     for (let index = 0; index < foods.length; index += 1) {
-//       console.log(foods[index][1]);
-//       console.log(drinks[index][1]);
-//       if (item === foods[index][0]) {
-//         pay += foods[index][1];
-//       }
-//       if (item === drinks[index][0]) {
-//         pay += drinks[index][1];
-//       }
-//     }
-//   });
-//   return pay * 1.1;
-// };
+  consun.forEach((item) => {
+    for (let index = 0; index < quantidadeDeItemCardapio; index += 1) {
+      if (item === foods[index][0]) {
+        pay += foods[index][1];
+      }
+      if (item === drinks[index][0]) {
+        pay += drinks[index][1];
+      }
+    }
+  });
+  return pay * 1.1;
+}
 
 function appendConsun(stg) {
   this.consumption.push(stg);
@@ -40,23 +29,9 @@ const createMenu = (cardapio) => {
     pay: function pagamento() {
       const foods = Object.entries(cardapio.food);
       const drinks = Object.entries(cardapio.drink);
-      const consumiveis = this.consumption; // ['coxinha', 'agua'];
+      const consumiveis = this.consumption;
 
-      let pay = 0;
-
-      consumiveis.forEach((item) => {
-        for (let index = 0; index < foods.length; index += 1) {
-          if (item === foods[index][0]) {
-            pay += foods[index][1];
-          }
-        }
-        for (let index = 0; index < drinks.length; index += 1) {
-          if (item === drinks[index][0]) {
-            pay += drinks[index][1];
-          }
-        }
-      });
-      return pay * 1.1;
+      return generatorPay(foods, drinks, consumiveis);
     },
   };
   return myObject;
