430200f4af9eaeba05a469b781c74bc55f39fe93
Marcelo Maurício <mmauricio.design@gmail.com>
2021-03-29 18:56:50 -0300

    Requisito 8 - alterada a funcao para gerar o array esperado no teste

--- src/myCounter.js
+++ src/myCounter.js
@@ -12,8 +12,8 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
+  for (var index = 0; index <= 3; index += 1) {
+    myArray.push(index);
     for (var counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
