7a8a2c89ce0889db640b17b76e305e5f2bb6fc18
Paulo H. Lopes <phlopes93@gmail.com>
2021-03-29 12:06:51 -0300

    requisitos 1 e 2

--- src/average.js
+++ src/average.js
@@ -12,6 +12,17 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-
+const average = (arrayNumbers) => {
+  let sum = 0;
+  let med = 0;
+  if (arrayNumbers.length === 0) {
+    return undefined;
+  }
+  for (let i = 0; i < arrayNumbers.length; i += 1) {
+    if (typeof arrayNumbers[i] !== 'number') return undefined;
+    sum += arrayNumbers[i];
+  }
+  med = sum / arrayNumbers.length;
+  return (Math.round(med));
+};
 module.exports = average;
--- src/myCounter.js
+++ src/myCounter.js
@@ -15,15 +15,15 @@
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
 
-const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
-    }
-  }
-  return myArray;
-};
+// const myCounter = () => {
+//   let myArray = [];
+//   for (let counter = 0; counter <= 3; counter += 1) {
+//     myArray.push(counter);
+//     for (let counter = 2; counter <= 3; counter += 1) {
+//       myArray.push(counter);
+//     }
+//   }
+//   return myArray;
+// };
 
-module.exports = myCounter;
+// module.exports = myCounter;
