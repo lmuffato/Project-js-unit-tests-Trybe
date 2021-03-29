c0d25f5556952d4d23a33fae5b65f1659aca7833
Ana Ventura <anaventura1811@gmail.com>
2021-03-29 15:24:11 -0300

    Corrige novamente os testes de circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -33,10 +33,20 @@ describe('#circle', () => {
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
-    assert.strictEqual(circle(2).circumference, parseFloat(2 * 3.14 * 2).toPrecision(2));
+    assert.strictEqual(circle(2).circumference, parseFloat(2 * 3.14 * 2).toPrecision(4));
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.strictEqual(circle(3).area, parseFloat(3.14 * 3 * 3).toPrecision(2));
+    assert.strictEqual(circle(3).area, parseFloat(3.14 * 3 * 3).toPrecision(4));
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    assert.strictEqual((circle(3), 3, 28.26, 18.84));
+    const circleRadius3 = circle(3);
+    circleRadius3.area = parseFloat(3.14 * 3 * 3).toPrecision(4);
+    circleRadius3.circumference = parseFloat(2 * 3.14 * 3).toPrecision(4);
+    assert.deepStrictEqual(circleRadius3, {
+      radius: 3,
+      area: 28.26,
+      circumference: 18.84,
+    });
   });
 });
+
+// Testes corrigidos, com base no pull request do colega Murilo Gonçalves
+// link: https://github.com/tryber/sd-010-a-project-js-unit-tests/pull/53/files
