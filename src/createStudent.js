9af4c0280b05e9c823db55b0c18d7a6179a4aa64
Guilherme de Prais <guilherme.prais@gmail.com>
2021-03-29 12:36:00 -0300

    requirement 05: turns feedback in arrowfunction

--- src/createStudent.js
+++ src/createStudent.js
@@ -18,9 +18,7 @@
 const createStudent = (nome) => {
   const student = {
     name: nome,
-    feedback() {
-      return 'Eita pessoa boa!';
-    },
+    feedback: () => 'Eita pessoa boa!',
   };
   return student;
 };
