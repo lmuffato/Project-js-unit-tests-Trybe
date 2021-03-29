a2bede2227acc0feb107381009090a3e8ee1754b
Marcelo Maurício <mmauricio.design@gmail.com>
2021-03-29 11:59:20 -0300

    Requisito 4 - criacao da metade dos testes

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,11 +25,11 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se circle retorna um objeto.
-    // Teste se o objeto retornado tem 3 entradas.
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(typeof circle, 'object'); // Teste se circle retorna um objeto.
+    assert.strictEqual(Object.keys(circle).length, 3); // Teste se o objeto retornado tem 3 entradas.
+    assert.strictEqual(circle(), undefined); // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
