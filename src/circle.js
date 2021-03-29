4a5b06956b8d4842d36704442f5ddec6164aac6b
lucas-paz <lucas.hpaz@outlook.com>
2021-03-29 14:35:31 -0300

    Fix circles function

--- src/circle.js
+++ src/circle.js
@@ -23,9 +23,9 @@ const circle = (radius) => {
   if (!radius) { return undefined; }
   return {
     radius,
-    area: Math.round(PI * radius * radius),
-    circumference: 2 * PI * radius,
+    area: parseFloat((PI * radius * radius).toPrecision(2)),
+    circumference: parseFloat((2 * PI * radius).toPrecision(2)),
   };
 };
-console.log(circle(3));
+
 module.exports = circle;
--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -22,7 +22,7 @@ const circle = require('../src/circle');
 
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
-const obj = { radius: 3, area: 28, circumference: 18.84 };
+const obj = { radius: 3, area: 28, circumference: 19 };
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
