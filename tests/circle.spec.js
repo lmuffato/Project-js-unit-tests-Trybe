82e66f03c5a15c11d0169cccfcf29532c28a8153
Ana Ventura <anaventura1811@gmail.com>
2021-03-29 16:35:28 -0300

    Testando mudança nos testes de circle

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -27,7 +27,7 @@ describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
-    assert.strictEqual(typeof (circle(1)), 'function');
+    assert.strictEqual(typeof circle(1), 'object');
     // Teste se o objeto retornado tem 3 entradas.
     assert.strictEqual(Object.entries(circle(1)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
