0ee76db5b6ff2cb2ca4d944a9a67c8f43accd7dc
Guilherme de Prais <guilherme.prais@gmail.com>
2021-03-29 09:40:27 -0300

    requirement 01

--- src/average.js
+++ src/average.js
@@ -12,8 +12,23 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // add your implementation here
+const average = (array) => {
+  let averageValues = 0;
+
+  if (array.length === 0) {
+    return undefined;
+  }
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    } else {
+      averageValues += array[i];
+    }
+  }
+
+  averageValues = Math.round(averageValues / array.length);
+  return averageValues;
 };
 
 module.exports = average;
