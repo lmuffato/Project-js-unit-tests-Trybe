938961cb08b3d2d38f939099a4fda2d045efb121
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-29 13:24:06 -0300

    Requisito 4 - revisado 4

--- src/circle.js
+++ src/circle.js
@@ -22,7 +22,7 @@ const circle = (radius) => {
   const PI = 3.14;
   if (!radius) { return undefined; }
   return {
-    radius,
+    radius: parseFloat(radius),
     area: parseFloat((PI * radius * radius).toPrecision(4)),
     circumference: parseFloat((2 * PI * radius).toPrecision(4)),
   };
