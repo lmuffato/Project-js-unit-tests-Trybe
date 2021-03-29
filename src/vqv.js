ef7f2dd5f007f3f42a577e190dc00b0951512b33
ThalitaC <thalitaceci@gmail.com>
2021-03-29 18:55:28 -0300

    Requisito 3: Implemente a função vqv

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,13 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (!nome || !idade) {
+    return undefined;
+  }
+  return (`Oi, meu nome é ${nome}!\nTenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!\n#VQV!`);
+
+};
 
 module.exports = vqv;
