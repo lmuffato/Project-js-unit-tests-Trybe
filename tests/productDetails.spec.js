79c0fdeb8c184c9b90961d09003c1faf12044594
André Jaques Batista <andrejaques.b@gmail.com>
2021-03-30 13:21:00 -0300

    fixing one write mistake

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -11,7 +11,7 @@ describe('#productDetails', () => {
     assert.strictEqual(typeof (funcFull[0]), 'object');
     assert.strictEqual(typeof (funcFull[1]), 'object');
     assert.strictEqual(funcFull[0] !== funcFull[1], true);
-    assert.strictEqual(funcFull[0].deatails.productId.slice(-3), '123');
-    assert.strictEqual(funcFull[1].deatails.productId.slice(-3), '123');
+    assert.strictEqual(funcFull[0].details.productId.slice(-3), '123');
+    assert.strictEqual(funcFull[1].details.productId.slice(-3), '123');
   });
 });
