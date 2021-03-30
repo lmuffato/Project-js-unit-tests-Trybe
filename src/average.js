c1b3a437bed6720a6cbb275b8077ab265c9078c1
diegoCorreia <diegomethal@gmail.com>
2021-03-30 16:00:10 -0300

    requisito 1 logica corrigida

--- src/average.js
+++ src/average.js
@@ -11,20 +11,18 @@
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
-function validateNumbersInArray(array) {
-  for (let index = 0; index < array.lenght; index += 1) {
-    if (typeof (array[index]) !== 'number' || array.lenght === 0) {
-      return false;
-    }
-  }
-}
-
 function average(array) {
-  if (validateNumbersInArray(array) === false) {
+if (array.length === 0){
+  return undefined
+}
+for (let numbers of array){
+  if (typeof numbers !== 'number'){
     return undefined;
   }
+}
   const sum = array.reduce((sumArray, result) => sumArray + result);
   return Math.round(sum / array.length);
 }
+console.log(average([1, 2, 4, 5, 9, 6]));
 
 module.exports = average;
