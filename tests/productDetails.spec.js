66e9e6e56cdaa0344c67be74fdab483aa0470d2b
André Jaques Batista <andrejaques.b@gmail.com>
2021-03-30 12:16:25 -0300

    add test if the function returns one array with two itens

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -9,7 +9,7 @@ describe('#productDetails', () => {
     assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     assert.strictEqual(Array.isArray(productDetails('Alcool gel', 'Máscara')), true);
-    // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
     // Teste que os dois objetos são diferentes entre si.
     // (Difícil) Teste que os dois productIds terminam com 123.
