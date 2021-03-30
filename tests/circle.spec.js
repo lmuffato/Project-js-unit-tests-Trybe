9fee1fe14419ebe7b4af4d48b30edb91d3de9fad
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-03-30 19:00:51 -0300

    reparos no lint

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -29,7 +29,6 @@ describe('#circle', () => {
     // Teste se circle retorna um objeto.
     assert.strictEqual(typeof circle, 'object');
     // Teste se o objeto retornado tem 3 entradas.
-    
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
