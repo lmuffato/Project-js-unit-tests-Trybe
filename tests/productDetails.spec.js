c4366d6c8bbacfed9805805ab418691e3a14f61f
André Jaques Batista <andrejaques.b@gmail.com>
2021-03-30 12:14:33 -0300

    add teste se o retorno e um array

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -4,38 +4,11 @@
 const assert = require('assert');
 const productDetails = require('../src/productDetails');
 
-/*
-  Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
-
-  Parâmetros:
-  - Uma string;
-  - Uma string;
-
-  Comportamento:
-  productDetails('Alcool gel', 'Máscara') // Retorna:
-  [
-    {
-      name: 'Alcool gel'
-      details: {
-        productId: 'Alcool gel123'
-      }
-    },
-    {
-      name: 'Máscara'
-      details: {
-        productId: 'Máscara123'
-      }
-    }
-  ]
-
-  OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
-*/
-
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
     assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
-    // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails('Alcool gel', 'Máscara')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
     // Teste que os dois itens dentro do array retornado pela função são objetos.
     // Teste que os dois objetos são diferentes entre si.
