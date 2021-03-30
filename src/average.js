6b7d0751a0fed8552975bc44874c8706266f1a75
biazidioti <bzferreira.bio@gmail.com>
2021-03-30 10:20:01 -0300

    Adiciona requisito 1.

--- src/average.js
+++ src/average.js
@@ -12,19 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
+const average = (array) => {
   let arraySum = 0;
 
   if (array.length === 0) {
     return undefined;
-  }
+  };
   for (let index = 0; index < array.length; index += 1) {
-    if (typeof array[index] !== 'number') {
+     if (typeof(array[index]) !== 'number') {
       return undefined;
     }
     arraySum += array[index];
   }
   return Math.round(arraySum / array.length);
-};
+}
 
 module.exports = average;
