6d5802fa9e50b2f739e9ca82d2924cdd52529c33
Matheus Lima Pimenta <mateus.krauss@gmail.com>
2021-03-30 17:20:49 -0300

    Tentando passar o requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,9 +16,16 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => `Oi, meu nome é ${nome}!
+const vqv = (nome, idade) => {
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
+  return `Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
   trabalho na Trybe e mando muito em programação!
   #VQV!`;
+};
+
+vqv();
 
 module.exports = vqv;
