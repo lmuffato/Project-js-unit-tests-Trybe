c4105beb420b1d28f0e8660f342107aa54682cb7
Matheus Lima Pimenta <mateus.krauss@gmail.com>
2021-03-30 20:04:43 -0300

    Remove o fail

--- src/vqv.js
+++ src/vqv.js
@@ -17,13 +17,14 @@
 */
 
 const vqv = (nome, idade) => {
-  if (nome === undefined || idade === undefined) {
-    return undefined;
-  }
-  return `Oi, meu nome é ${nome}!
+  const messageVqv = `Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
   trabalho na Trybe e mando muito em programação!
   #VQV!`;
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
+  return messageVqv;
 };
 
 console.log(vqv());
