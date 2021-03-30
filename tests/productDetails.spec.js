0f0b1b787624667211b3c30ee7870195144643f2
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-03-30 09:51:21 -0300

    Implemente os casos de teste para a função productDetails not done

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,17 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste que o retorno da função é um array.
-    // Teste que o array retornado pela função contém dois itens dentro.
-    // Teste que os dois itens dentro do array retornado pela função são objetos.
-    // Teste que os dois objetos são diferentes entre si.
+    const products = productDetails('Creme de cabelo', 'Creme dental');
+
+    assert.strictEqual(Array.isArray(products), true);
+    assert.strictEqual(products.length, 2);
+    assert.strictEqual(typeof products[0], 'object');
+    assert.strictEqual(typeof products[1], 'object');
+    assert.notDeepStrictEqual(products[0], products[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    // const id1 = products[0].details.productId;
+    // const id2 = products[1].details.productId;
+    // assert.strictEqual(id1.slice(-3), '124');
+    // assert.strictEqual(id2.slice(-3), '124');
   });
 });
