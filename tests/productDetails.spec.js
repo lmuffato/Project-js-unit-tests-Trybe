53289b52d270adc71c69cd2eff1b5e228f5bd38a
Vinicios Fraga <soicinivt@gmail.com>
2021-03-29 23:54:47 -0300

    Requisito 6.

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,13 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
-    // Teste que o retorno da função é um array.
-    // Teste que o array retornado pela função contém dois itens dentro.
-    // Teste que os dois itens dentro do array retornado pela função são objetos.
-    // Teste que os dois objetos são diferentes entre si.
-    // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(typeof (productDetails()), 'object');// Teste que o retorno da função é um array.
+    assert.strictEqual(productDetails().length, 2);// Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(typeof (productDetails()[0]) && typeof (productDetails()[1]), 'object');// Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.notStrictEqual(productDetails()[0], productDetails()[1], 'Erro: objetos iguais');// Teste que os dois objetos são diferentes entre si.
+    const firstProductId = Object.values(Object.values(productDetails()[0])[1])[0].slice(-3); // Referencia : https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/slice
+    const secondProductId = Object.values(Object.values(productDetails()[1])[1])[0].slice(-3);
+    assert.strictEqual(firstProductId && secondProductId, '123');// (Difícil) Teste que os dois productIds terminam com 123.
   });
 });
