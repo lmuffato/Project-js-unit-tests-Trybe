da3ce4a19819afd20bf4c20e0c7672e968e94635
biazidioti <bzferreira.bio@gmail.com>
2021-03-30 14:00:00 -0300

    Arruma lint do requisito 6 de novo.

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -37,7 +37,7 @@ describe('#productDetails', () => {
     // Teste que o retorno da função é um array.
     assert.strictEqual(typeof(productDetails('tabaco', 'seda')), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
-    assert.strictEqual((productDetails('tabaco', 'seda').length), 2);
+    assert.strictEqual(productDetails('tabaco', 'seda').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
 
     // Teste que os dois objetos são diferentes entre si.
