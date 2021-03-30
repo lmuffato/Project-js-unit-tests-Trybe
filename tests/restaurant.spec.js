60fe0206dffb61a5ba3f64e036480c32a73fe082
rogerio <rogerio.cl@gmail.com>
2021-03-29 22:41:20 -0300

    resolvendo problema no teste requisito 9

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -52,12 +52,12 @@ const createMenu = require('../src/restaurant');
 
 describe('#createMenu', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.strictEqual(typeof createMenu().fetchMenu, 'function');
-    assert.deepStrictEqual(Object.keys(createMenu().fetchMenu()), ['food', 'drink']);
     const menu = {
       food: { coxinha: 3.90, sanduiche: 9.90, sopa: 12, sashimi: 20 },
       drink: { agua: 3.90, cerveja: 6.90 },
     };
+    assert.strictEqual(typeof createMenu(menu).fetchMenu, 'function');
+    assert.deepStrictEqual(Object.keys(createMenu(menu).fetchMenu()), ['food', 'drink']);
     let restaurant = createMenu(menu);
     assert.deepStrictEqual(createMenu(menu).fetchMenu(), menu);
     assert.deepStrictEqual(createMenu(menu).consumption, []);
