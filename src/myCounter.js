9388f25e1c177ba651d7983c8e4fe9410c781709
biazidioti <bzferreira.bio@gmail.com>
2021-03-30 15:49:11 -0300

    Arruma identacao.

--- src/myCounter.js
+++ src/myCounter.js
@@ -14,11 +14,11 @@ const myCounter = () => {
   var myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-  for (let otherCounter = 2; otherCounter <= 3; otherCounter += 1) {
-    myArray.push(otherCounter);
+    for (let otherCounter = 2; otherCounter <= 3; otherCounter += 1) {
+      myArray.push(otherCounter);
     }
   }
-    return myArray;
+  return myArray;
 };
 
 module.exports = myCounter;
