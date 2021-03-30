ef4628ebb125e2521294182f3c37a221ef855876
André Jaques Batista <andrejaques.b@gmail.com>
2021-03-30 12:47:04 -0300

    fixing lint inssues

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,15 +1,14 @@
-/* eslint-disable max-len */
-/* eslint-disable no-unused-vars */
-
 const assert = require('assert');
+
 const productDetails = require('../src/productDetails');
+
 const funcFull = (productDetails('Álcool gel', 'Máscara'));
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
     assert.strictEqual(Array.isArray(funcFull), true);
     assert.strictEqual(funcFull.length, 2);
-    assert.strictEqual(typeof(funcFull[0]) && typeof(funcFull[1]), 'object');
+    assert.strictEqual(typeof (funcFull[0]) && typeof (funcFull[1]), 'object');
     assert.strictEqual(funcFull[0] !== funcFull[1]);
     assert.strictEqual(funcFull[0].slice(-3) && funcFull[1].slice(-3), '123');
   });
