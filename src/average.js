e2d1e67708c8c3c1747950e764d08c0b789242c4
JuliaBaptista <juliabaptistadeoliveira@gmail.com>
2021-03-29 22:31:01 -0300

    Correçoes requisito 1 para aprovaçao ESlint

--- src/average.js
+++ src/average.js
@@ -14,16 +14,16 @@
 
 const average = (array) => {
   let sum = 0;
+  if (array ===0) {
+    return undefined;
+  }
   for (let index = 0; index < array.length; index += 1) {
     if (typeof array[index] !== 'number') {
       return undefined;
-      }
+    }
     sum += array[index];
   }
   return Math.round(sum / array.length);
-  if (array === 0) {
-    return undefined;
-  }
 };
 
 console.log(average([2, 2]));
