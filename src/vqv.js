6bfec995b1028dec4c9d0b6101a3e1eb5f35c881
biazidioti <bzferreira.bio@gmail.com>
2021-03-30 10:50:45 -0300

    Adiciona requisito 3.

--- src/vqv.js
+++ src/vqv.js
@@ -17,12 +17,13 @@
 */
 
 const vqv = (nome, idade) => {
-  if(typeof(nome) === 'sting' && typeof(number) === 'number') {
-  `Oi, meu nome é ${nome}!
+  if (nome === undefined && idade === undefined) {
+    return undefined;
+  }
+  return `Oi, meu nome é ${nome}!
   Tenho ${idade} anos,
   trabalho na Trybe e mando muito em programação!
   #VQV!`
-  }
 };
 
 module.exports = vqv;
