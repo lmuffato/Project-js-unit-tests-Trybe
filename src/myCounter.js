b2f9c86979449c5eeeeae20baeac5e066c543e9d
biazidioti <bzferreira.bio@gmail.com>
2021-03-30 15:47:43 -0300

    Agora vai requisito 8 DE NOVO.

--- src/myCounter.js
+++ src/myCounter.js
@@ -12,12 +12,8 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-  const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-  for (var otherCounter = 2; otherCounter <= 3; otherCounter += 1) {
-    myArray.push(otherCounter);
   for (let otherCounter = 2; otherCounter <= 3; otherCounter += 1) {
     myArray.push(otherCounter);
     }
