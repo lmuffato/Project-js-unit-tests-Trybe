f3cfa4216695e027b6461182f8431bc190b78414
Matheus Lima Pimenta <mateus.krauss@gmail.com>
2021-03-30 19:59:12 -0300

    testa circle

--- src/circle.js
+++ src/circle.js
@@ -23,9 +23,9 @@ const circle = (radius) => {
   if (!radius) { return undefined; }
   return {
     radius,
-    area: PI * radius * radius,
+    area: parseFloat(PI * radius * radius).toPrecision(2),
     circumference: 2 * PI * radius,
   };
 };
-
+console.log(circle(3));
 module.exports = circle;
