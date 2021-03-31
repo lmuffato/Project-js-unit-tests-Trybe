9c0d43bb78eb60fd5f596655578998f71688d505
Samuel Melo <samuelmmjunior@gmail.com>
2021-03-31 18:26:14 -0300

    requisito 1

--- src/average.js
+++ src/average.js
@@ -12,8 +12,24 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (array) => {
+  let media = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+
+  for (let index = 0;index < array.length; index += 1) {
+      if (typeof array[index] !== 'number') {
+        return undefined
+      }
+      media += array[index];    
+  }
+  // return Math.round(media / array.length); 
+  media = Math.round(media / array.length);
+  return media;
 };
+
+
+
 
 module.exports = average;
