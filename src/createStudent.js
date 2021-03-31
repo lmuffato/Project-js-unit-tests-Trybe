89cc21e4e44d74232f47e090ac779301ba61b858
Paulo H. Lopes <phlopes93@gmail.com>
2021-03-31 17:14:55 -0300

    requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const obj = {
+    name: `${nome}`,
+    feedback: () => (('Eita pessoa boa!')),
+  }; return obj;
+};
 
 module.exports = createStudent;
