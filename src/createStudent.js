678269df28cf069ee96e6966f2056a44878c8b86
luanlsr <luanfswd@gmail.com>
2021-03-31 11:43:04 -0300

    finalizado a correçao dos erros de lint no req. 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -18,7 +18,7 @@
 const createStudent = (name) => {
   const newObject = {
     nome: name, 
-    feedback: () => 'Eita pessoa boa!';
+    feedback: () => 'Eita pessoa boa!'
   };
   return newObject;
 };
