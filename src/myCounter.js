b2085ce1db924652e590855f1fef2f210512f519
Hernanne <hernannedomiciano@gmail.com>
2021-03-31 15:54:12 -0300

    requisito 8 feito

--- src/myCounter.js
+++ src/myCounter.js
@@ -20,10 +20,10 @@ function myCounter() {
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (let counter1 = 2; counter1 <= 3; counter1 += 1) {
-      myArray.push(counter1)
+      myArray.push(counter1);
     }
   }
   return myArray;
 }
-console.log(myCounter())
+console.log(myCounter());
 module.exports = myCounter;
