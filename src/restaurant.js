880347b8055ca1b0156a26682a3679d5ee34c020
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-03-31 13:24:27 -0300

    Requisito 12 agora finalmente concluído

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,22 +1,58 @@
 /* eslint-disable max-len */
 
-function createItemsMenu(items) {
+function createOrder(items) {
   this.consumption.push(items);
+}
+
+function totalPrice() {
+  let total = 0;
+  let totalFood = 0;
+  let totalDrink = 0;
+  let totalPercent = 0;
+  const percent = 0.10;
+  const consumptionArray = this.consumption;
+  const menu = this.fetchMenu();
+
+  for (let index = 0; index < consumptionArray.length; index += 1) {
+    const element = consumptionArray[index];
+
+    if (menu.food[element] !== undefined) {
+      totalFood += menu.food[element];
+    }
+    if (menu.drink[element] !== undefined) {
+      totalDrink += menu.drink[element];
+    }
+  }
+
+  total = totalFood + totalDrink;
+  totalPercent = total + (total * percent);
+  return totalPercent;
 }
 
 const createMenu = (objectMenu) => {
   const menu = {
     fetchMenu: () => objectMenu,
     consumption: [],
-    order: createItemsMenu,
+    order: createOrder,
+    pay: totalPrice,
   };
   return menu;
 };
 
-const objetoRetornado = createMenu({ food: {}, drink: {} });
+// const objetoRetornado = createMenu({ food: { coxinha: 2, sopa: 4 }, drink: { agua: 2, suco: 3 } });
+// console.log(objetoRetornado.fetchMenu());
+// objetoRetornado.order('coxinha');
+// objetoRetornado.order('sopa');
+// objetoRetornado.order('agua');
+// console.log(objetoRetornado.consumption);
+// console.log(objetoRetornado.pay());
 
+// const objectFoodAndDrink = createMenu({ food: { coxinha: 2, sopa: 4 }, drink: { agua: 2, suco: 3 } });
+// console.log(objectFoodAndDrink.fetchMenu().food);
+// console.log(Object.keys(objetoRetornado.fetchMenu()).includes('food', 'drink'));
 // console.log(objetoRetornado.order('items'));
-objetoRetornado.order('items');
-console.log(objetoRetornado.consumption);
+
+// objetoRetornado.order('items');
+// console.log(objetoRetornado.consumption);
 
 module.exports = createMenu;
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -14,20 +14,37 @@ const consumptionItems = (quantity) => {
   }
 };
 
+const includeKeys = (object, keys) => {
+  const objKeys = Object.keys(object.fetchMenu());
+  return objKeys.includes(keys[0]) && objKeys.includes(keys[1]);
+};
+
 describe('#createMenu', () => {
   it('tests the function has the correct behaviour', () => {
+    // TESTE 1:
     assert.deepStrictEqual(typeof (createMenu()), 'object');
-    assert.deepStrictEqual(objetoRetornado.fetchMenu(), { food: {}, drink: {} });
+    // TESTE 2:
+    assert.strictEqual(includeKeys(objetoRetornado, ['food', 'drink']), true);
+    // TESTE 3:
     assert.deepStrictEqual({ food: {}, drink: {} }, objetoRetornado.fetchMenu());
+    // TESTE 4:
     assert.deepStrictEqual(objetoRetornado.consumption, []);
+    // TESTE 5:
     consumptionItems(1);
     assert.deepStrictEqual(objetoRetornado.consumption, ['coxinha']);
+    // TESTE 6:
     objetoRetornado.consumption = [];
     consumptionItems(4);
     assert.deepStrictEqual(objetoRetornado.consumption, ['coxinha', 'agua', 'sopa', 'sashimi']);
+    // TESTE 7:
     objetoRetornado.consumption = [];
     consumptionItems(5);
     assert.deepStrictEqual(objetoRetornado.consumption,
       ['coxinha', 'agua', 'sopa', 'sashimi', 'sashimi']);
+    // TESTE 8:
+    const objectFoodAndDrink = createMenu({ food: { coxinha: 2, sopa: 4 }, drink: { agua: 2, suco: 3 } });
+    objectFoodAndDrink.consumption = ['coxinha', 'sopa', 'agua'];
+    const total = 8 + (8 * 0.10);
+    assert.strictEqual(objectFoodAndDrink.pay(), total);
   });
 });
