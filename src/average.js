9bbeccf44295bb93f351bd090f80d349936ee80a
root <root@DESKTOP-74CRLKE.localdomain>
2021-03-31 18:35:35 -0300

    requisito 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-
+const average = (array) => {
+  let sum = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number') {
+      return undefined;
+    }
+    sum += array[index];
+  }
+  let result = sum / array.length;
+  return Math.round(result);
+};
+console.log(average([1, 2, 3]));
 module.exports = average;
