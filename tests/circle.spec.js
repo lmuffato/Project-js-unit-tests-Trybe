8d77b342c4a22137df30200f946d663898ce16d9
luanlsr <luanfswd@gmail.com>
2021-03-31 11:08:18 -0300

    corrigido erros de lint

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -31,6 +31,6 @@ describe('#circle', () => {
     assert.strictEqual(circle(), undefined);
     assert.strictEqual(circle(2).circumference, 12.56);
     assert.strictEqual(circle(3).circumference, 18.84);
-    assert.deepStrictEqual(circle(3), { radius: 3, area: 28.2599999999, circumference: 18.84 });
+    assert.deepStrictEqual(circle(3), { radius: 3, area: 28.259999999999998, circumference: 18.84 });
   });
 });
