efe96e40e7053b8d5facd2eff1a30ed83fb82f65
Matheus Lima Pimenta <mateus.krauss@gmail.com>
2021-03-31 20:59:57 -0300

    remove erro

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,7 +33,6 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     const prods = productDetails('Alcool gel', 'Máscara');
     // Teste que o retorno da função é um array.
