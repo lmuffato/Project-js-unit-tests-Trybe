6220277312b2eabd1bed69cc99749129f1a5d2fd
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-31 14:44:38 -0300

    Requisito 7 revisado (1)

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,22 +19,14 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator =  {
-  add: somando = (number1, number2) => {
-    return number1 + number2;
-  },
+const calculator = {
+  add: somando = (number1, number2) => number1 + number2,
 
-  mult: multiplicacao = (number1, number2) => {
-    return number1 * number2;
-  },
+  mult: multiplicacao = (number1, number2) => number1 * number2,
 
-  div: dividindo = (number1, number2) => {
-    return Math.floor(number1 / number2);
-  },
+  div: dividindo = (number1, number2) => Math.floor(number1 / number2),
 
-  sub: subtraindo = (number1, number2) => {
-    return number1 - number2;
-  }
+  sub: subtraindo = (number1, number2) => number1 - number2,
 };
 
 module.exports = calculator;
