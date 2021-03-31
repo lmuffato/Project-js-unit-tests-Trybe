1ee9809b00d7c841712d647f717584516022f168
ThalitaC <thalitaceci@gmail.com>
2021-03-31 17:02:56 -0300

    Requisito 6: Implemente os casos de teste para a função productDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,19 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    // found about .isArray at https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+    assert.strictEqual(Array.isArray(productDetails()), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof (productDetails('first', 'second')), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(productDetails('first'), productDetails('second'), true);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    // found about the .endsWith method at https://www.w3schools.com/jsref/jsref_endswith.asp#:~:text=The%20endsWith()%20method%20determines,()%20method%20is%20case%20sensitive.
+    assert.ok(productDetails()[0].details.productId.endsWith('123'));
+    assert.ok(productDetails()[1].details.productId.endsWith('123'));
   });
 });
