1ae8960f6abcb97b5288f8ce81119ce839deb0c5
JuliaBaptista <juliabaptistadeoliveira@gmail.com>
2021-03-31 19:34:18 -0300

    Requisito 2 - alteraçoes arquivo test

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -15,16 +15,17 @@ const numbers = require('../src/numbers');
 
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
+    // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+    // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+    // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+    // Escreva um teste em que a função recebe [' '] e retorna false
+
 
 describe('#numbers', () => {
   it('should return an array and return if it has only numbers or not', () => {
-    // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
     assert.strictEqual(numbers([1, 2, 3, 4, 5]), true);
-    // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
     assert.strictEqual(numbers([1, 2, '3', 4, 5]), false);
-    // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
     assert.strictEqual(numbers([1, 'a', 3]), false);
-    // Escreva um teste em que a função recebe [' '] e retorna false
     assert.strictEqual(numbers([' ']), false);
   });
 });
