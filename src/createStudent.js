01716da183b029637c07707438bcc20d0ed421e0
Vinicius Rodrigues <V.rodriguesjk@gmail.com>
2021-03-31 13:52:31 -0300

    Requisito 5: Retorna uma funçao como valor da chave

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,14 +14,10 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
-const returnFeedback = () => {
-  return 'Eita pessoa boa!';
-};
+const returnFeedback = () => 'Eita pessoa boa!';
 
-const createStudent = (student) => {
-  return {
-    name: student,
-    feedback: returnFeedback,
-  };
-};
+const createStudent = (student) => ({
+  name: student,
+  feedback: returnFeedback,
+});
 module.exports = createStudent;
