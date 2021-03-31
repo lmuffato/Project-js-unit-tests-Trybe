5c04eabef39f254c07abdfaac00bd6536702f5a0
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-03-31 02:16:30 -0300

    Requisito 6 / tentativa 2 - isArray

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -36,7 +36,7 @@ describe('#productDetails', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     const products = productDetails('Alcool gel', 'Máscara');
     // Teste que o retorno da função é um array.
-    assert.strictEqual(typeof products, 'array');
+    assert.strictEqual(Array.isArray(products), true);
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(products.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
