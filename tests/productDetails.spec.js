5309250279678deab2720b99c096e4235d1d543e
Tiago Santos <tiago881@gmail.com>
2021-04-01 19:46:25 -0300

    6 - Implemente os casos de teste para a função productDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,7 +33,30 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.strictEqual(Array.isArray(productDetails('firstProduct', 'secondProduct')), true);
+    assert.strictEqual(
+      Array.isArray(productDetails('firstProduct', 'secondProduct')),
+      true,
+    );
+    assert.strictEqual(productDetails('param1', 'param2').length, 2);
+    const firstProduct = productDetails('param1', 'param2')[0];
+    const secondProduct = productDetails('param1', 'param2')[1];
+    assert.ok(
+      Object.keys(firstProduct).length >= 1
+        && Object.keys(secondProduct).length >= 1,
+    );
+    assert.notDeepStrictEqual(
+      firstProduct,
+      secondProduct,
+      'Os objetos não são diferentes entre si',
+    );
+    const prod1 = firstProduct.details.productId;
+    const prod2 = secondProduct.details.productId;
+    const prodSize1 = prod1.length;
+    const prodSize2 = prod2.length;
+
+    assert.ok(prod1.slice(prodSize1 - 3, prodSize1) === '123');
+    assert.ok(prod2.slice(prodSize2 - 3, prodSize2) === '123');
+
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
     // Teste que o array retornado pela função contém dois itens dentro.
