597ceffb7049a2a04f3d4de346fcb835325d13e1
André Jaques Batista <andrejaques.b@gmail.com>
2021-04-01 10:28:05 -0300

    fix require 06 Math.roundto floor and add require 07

--- src/myCounter.js
+++ src/myCounter.js
@@ -1,20 +1,8 @@
-/*
-  A função myCounter possui dois loops aninhados que inserem valores dentro de um array.
-  Como podemos perceber, eles vão adicionando valores ao array até sua condição de parada.
-  Corrija o código abaixo para que a função retorne o array correto.
-
-  Parâmetros:
-  - Nenhum.
-
-  Comportamento:
-  myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
-*/
-
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  let myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
+    for (let counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
@@ -22,3 +10,4 @@ const myCounter = () => {
 };
 
 module.exports = myCounter;
+console.log(myCounter());
--- tests/myCounter.spec.js
+++ tests/myCounter.spec.js
@@ -1,5 +1,3 @@
-/* eslint-disable max-len */
-
 const assert = require('assert');
 const myCounter = require('../src/myCounter');
 
