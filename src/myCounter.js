8a9457fde90eb7b9d75ece1ceaa65f805c005c5d
nascjoao <oi@joaonasc.dev>
2021-04-01 14:06:04 -0300

    Requisito 8: Função myCounter.js

--- src/myCounter.js
+++ src/myCounter.js
@@ -12,10 +12,10 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let subCounter = 2; subCounter <= 3; subCounter += 1) {
+      myArray.push(subCounter);
     }
   }
   return myArray;
