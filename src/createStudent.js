226f1b9e233288e4c9760e2b980f50ebdf672696
Debora Passos <deborinhapassos@gmail.com>
2021-04-01 18:07:31 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  let studant = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return studant;
+};
 
 module.exports = createStudent;
