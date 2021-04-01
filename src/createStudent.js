4b4283ed81ca8464f4b1f48be2d50324eff10d8d
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2021-04-01 14:46:08 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,7 +14,11 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
-
-const createStudent = () => {};
-
+const createStudent = (name) => {
+  const pessoaEstudante = {
+    nome: name,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return pessoaEstudante;
+};
 module.exports = createStudent;
