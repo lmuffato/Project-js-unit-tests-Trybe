85a1ff931a3a683cf8bda2921f9bba23cc0cd8a0
Debora Passos <deborinhapassos@gmail.com>
2021-04-01 17:52:46 -0300

    Requisito 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,13 +25,30 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
+    assert.strictEqual(typeof circle(1), 'object');
+
     // Teste se o objeto retornado tem 3 entradas.
+    assert.strictEqual(Object.entries(circle(1)).length, 3);
+
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(circle(), undefined);
+
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert.deepStrictEqual(parseFloat(circle(2).circumference.toPrecision(4)), 12.56);
+
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.deepStrictEqual(parseFloat(circle(3).area.toPrecision(4)), 28.26);
+
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    const dadosCorretos = circle(3);
+    dadosCorretos.area = parseFloat(circle(3).area.toPrecision(4));
+    assert.deepStrictEqual(dadosCorretos, {
+      radius: 3,
+      area: 28.26,
+      circumference: 18.84,
+    });
   });
 });
+// No ultimo teste, analisei a lógica do código do colega Rafael, pois não estava conseguindo entender como utilizar o parseFloat dentro de mais de um elemento e uma vez
