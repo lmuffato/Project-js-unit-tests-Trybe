eb3422f7783b57efff413babd9dbb73520033dc9
root <root@DESKTOP-74CRLKE.localdomain>
2021-03-31 23:28:38 -0300

    requisito 3

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -25,6 +25,6 @@ describe('#numbers', () => {
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
     assert.strictEqual(numbers([1, 'a', 3]), false);
     // Escreva um teste em que a função recebe [' '] e retorna false
-    assert.strictEqual(numbers(['']), false);
+    assert.strictEqual(numbers([' ']), false);
   });
 });
