7e2bef5cd389744cacfcf305f55eb622856aeb71
Debora Passos <deborinhapassos@gmail.com>
2021-04-01 15:56:56 -0300

    Requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (!nome && !idade) {
+    return undefined;
+  }
+  const salude = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 
+  return salude;
+};
 module.exports = vqv;
