847985e40e17372ed2a88acf82ea5fb4153998ad
Samuel Melo <samuelmmjunior@gmail.com>
2021-04-01 16:17:47 -0300

    requisito 7

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -18,7 +18,13 @@
   calculator.add(1, 1) // Retorno: 2;
   calculator.div(3, 2) // Retorno: 1;
 */
+// Requisito resolvido com ajuda de Guilherme Prais 
 
-const calculator = {};
+const calculator = {
+  add: (num1, num2) => Math.floor(num1 + num2),
+  mult: (num1, num2) => Math.floor(num1 * num2),
+  div: (num1, num2) => Math.floor(num1 / num2),
+  sub: (num1, num2) => Math.floor(num1 - num2),
+};
 
 module.exports = calculator;
