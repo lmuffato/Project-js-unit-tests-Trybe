6ec1d5026971616307f793dae0363fb81323354f
Samuel Melo <samuelmmjunior@gmail.com>
2021-04-01 12:37:14 -0300

    requisito 4 correção do Eslint

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -22,7 +22,7 @@ const circle = require('../src/circle');
 
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
-    // Este teste foi desenvolvido com base nos testes do estudante Guilherme Prais/Sergio MArtins
+// Este teste foi desenvolvido com base nos testes do estudante Guilherme Prais/Sergio MArtins
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
     // assert.fail();
