05161e6b360402346ff673e48f454bf889035669
root <root@DESKTOP-74CRLKE.localdomain>
2021-04-01 15:44:53 -0300

    requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -14,11 +14,11 @@ const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (var counterTwo = 2; counterTwo <= 3; counterTwo += 1) {
+      myArray.push(counterTwo);
     }
   }
   return myArray;
 };
-
+console.log(myCounter());
 module.exports = myCounter;
