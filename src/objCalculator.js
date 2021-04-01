b5eb464b49d977cd2eb6ca9ab9679453ff8217ed
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-04-01 16:34:13 -0300

    Correcao de erros ESlint requisito 7

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -21,9 +21,9 @@
 
 const calculator = {
   add: (a, b) => Math.floor(a + b),
-  mult:(a, b) => Math.floor(a * b),
+  mult: (a, b) => Math.floor(a * b),
   div: (a, b) => Math.floor(a / b),
-  sub: (a, b) => Math.floor(a - b)
+  sub: (a, b) => Math.floor(a - b),
 };
 
 module.exports = calculator;
