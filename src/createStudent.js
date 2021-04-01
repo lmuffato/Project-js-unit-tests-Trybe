558d53652e2d6a6c74631af1d8031b011fa0d74e
Tiago Santos <tiago881@gmail.com>
2021-04-01 15:50:36 -0300

    5 - Implemente a função createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (param) => ({
+  name: param,
+  feedback: () => 'Eita pessoa boa!',
+});
+
+const estudante = createStudent('Leandrão');
 
 module.exports = createStudent;
