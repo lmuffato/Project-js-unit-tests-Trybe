892e6f7db704dae05813779ffb3324753f424abb
André Jaques Batista <andrejaques.b@gmail.com>
2021-04-01 10:19:27 -0300

    add require 07 - objCalculator

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,15 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
+const calculator = {
+  add: (a, b) => a + b,
+  mult: (a, b) => a * b,
+  div: (a, b) => Math.round(a / b),
+  sub: (a, b) => a - b,
+};
 
 module.exports = calculator;
+console.log(calculator.add(3, 7));
+console.log(calculator.sub(3, 7));
+console.log(calculator.mult(3, 7));
+console.log(calculator.div(3, 7));
