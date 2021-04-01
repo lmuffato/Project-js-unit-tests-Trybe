4867fdf10b25cd4ed5397b16370c7be0ee26ba9e
nascjoao <oi@joaonasc.dev>
2021-03-31 23:07:38 -0300

    Converte retorno de string para number

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -36,7 +36,7 @@ describe('#circle', () => {
     assert.strictEqual(Object.entries(circle(1)).length, 3);
     assert.strictEqual(circle(), undefined);
     assert.strictEqual(circle(2).circumference, 12.56);
-    assert.strictEqual(circle(3).area.toPrecision(4), 28.26);
+    assert.strictEqual(Number(circle(3).area.toPrecision(4)), 28.26);
     assert.deepStrictEqual(circle(3), {
       radius: 3,
       area: 28.259999999999998,
