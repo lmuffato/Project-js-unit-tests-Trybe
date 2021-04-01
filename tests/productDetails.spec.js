6e8e0b1497d8f76adf9f73a5b4fa184382078eb3
Rodrigo Grande <rogrande0496@gmail.com>
2021-04-01 18:23:45 -0300

    completa requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,18 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof productDetails, 'array');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('x', 'y').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof productDetails('x', 'y')[0]
+    && typeof productDetails('x', 'y')[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(productDetails('x', 'y')[0], productDetails('x', 'y')[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.deepStrictEqual(productDetails('x', 'y')[0].details.productId.substr(1), '123');
+    assert.deepStrictEqual(productDetails('x', 'y')[1].details.productId.substr(1), '123');
   });
 });
