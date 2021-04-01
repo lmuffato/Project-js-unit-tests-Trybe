a25ef118cce160e788c7c4c9ab3aedc9ff8e41ab
Rodrigo Grande <rogrande0496@gmail.com>
2021-04-01 17:56:43 -0300

    completa requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const feedback = () => 'Eita pessoa boa!';
+  const student = {
+    name,
+    feedback,
+  };
+  return student;
+};
 
+console.log(createStudent('Rodrigo'));
 module.exports = createStudent;
