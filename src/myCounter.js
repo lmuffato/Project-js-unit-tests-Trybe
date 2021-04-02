53426234ddb6981543835e455497bca167fe3dc7
Douglas <douguinhodns@gmail.com>
2021-04-01 21:08:25 -0300

    requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -11,11 +11,11 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  let myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let otherCounter = 2; otherCounter <= 3; otherCounter += 1) {
+      myArray.push(otherCounter);
     }
   }
   return myArray;
