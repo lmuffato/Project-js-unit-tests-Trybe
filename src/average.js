56f299b5d6483b132e51fbf981e82ecd49ea17f1
Southier <patrick.southier@hotmail.com>
2021-04-01 22:22:28 -0300

    Completes requirement 1.

--- src/average.js
+++ src/average.js
@@ -3,7 +3,6 @@
   Caso a função receba algum valor não númerico ou um array vazio,
   o valor undefined deve ser retornado.
   Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
- 
   Parâmetros:
     - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
   Comportamento:
@@ -12,6 +11,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (arr) => {
+  let total = 0;
+  for (let index = 0; index < arr.length; index += 1) {
+    const typeCheck = arr[index];
+    if (typeof typeCheck !== 'number') {
+      return undefined;
+    }
+    total += typeCheck;
+  }
+  if (arr.length === 0) {
+    return undefined;
+  }
+  return Math.round(total / arr.length);
+};
 
 module.exports = average;
