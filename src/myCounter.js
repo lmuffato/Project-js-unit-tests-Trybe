a974ce34df2ee672d9e3adf2f514879eeefd150a
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-04-02 15:01:24 -0300

    Requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -13,18 +13,17 @@
 
   Comportamento:
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
-
+*/
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  let myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
+      myArray.push(counter2);
     }
   }
   return myArray;
 };
+console.log(myCounter());
 
 module.exports = myCounter;
-
-*/
