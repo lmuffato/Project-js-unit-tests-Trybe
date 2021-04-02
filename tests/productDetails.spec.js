d1421d3a8c2d51978b8604bc9688d65c577dd586
Rodrigo Grande <rogrande0496@gmail.com>
2021-04-02 14:36:30 -0300

    fix requisito 7

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -42,9 +42,9 @@ describe('#productDetails', () => {
     assert.deepStrictEqual(typeof productDetails('x', 'y')[0]
     && typeof productDetails('x', 'y')[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
-    assert.notDeepStrictEqual(productDetails('x', 'y')[0], productDetails('x', 'y')[1]);
+    assert.notStrictEqual(productDetails('x', 'y')[0], productDetails('x', 'y')[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
-    assert.deepStrictEqual(productDetails('x', 'y')[0].details.productId.substr(1), '123');
-    assert.deepStrictEqual(productDetails('x', 'y')[1].details.productId.substr(1), '123');
+    assert.strictEqual(productDetails('x', 'y')[0].details.productId.slice(-3), '123');
+    assert.strictEqual(productDetails('x', 'y')[1].details.productId.slice(-3), '123');
   });
 });
