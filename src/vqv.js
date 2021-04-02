7162dbfec5ebb9c02f687763d8155fe0c3e9df45
Priscila <prihscapin@gmail.com>
2021-04-02 12:14:26 -0300

    Requisito 2 - VQV

--- src/vqv.js
+++ src/vqv.js
@@ -15,7 +15,14 @@
       trabalho na Trybe e mando muito em programação!
       #VQV!'
 */
+const vqv = (nome, idade) => {
+  if (!nome && !idade) return undefined;
 
-const vqv = (nome, idade) => {};
+  const message = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 
+  return message;
+};
 module.exports = vqv;
