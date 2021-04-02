369bccd442834aaa1fc6f9eece425971722d5e41
Priscila <prihscapin@gmail.com>
2021-04-02 14:22:35 -0300

    Requisito 5 - createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const feedback = () => 'Eita pessoa boa!';
+  const estudante = {
+    name: nome,
+    feedback: feedback(),
+  };
+};
 
 module.exports = createStudent;
