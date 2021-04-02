3488936ea05537f14e6e6fdfc611c206a54c451a
pmaiamateus <pmaiamateus@gmail.com>
2021-04-02 11:21:02 -0300

    conserta average 4.0

--- tests/average.spec.js
+++ tests/average.spec.js
@@ -19,10 +19,10 @@ const average = require('../src/average');
 */
 function avg(array) {
   let sum = 0;
-  if (array.length === 0) {
-    return undefined;
-  }
   for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number') {
+      return undefined;
+    }
     sum += array[index];
   }
   return Math.round(sum / array.length);
