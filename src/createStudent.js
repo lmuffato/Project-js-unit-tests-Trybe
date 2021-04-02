d22f412d64a38d46cf3cb79e17cc302d2e840add
Moises-Coimbra <moisescoimbra.digitalpro@gmail.com>
2021-04-02 11:28:40 -0300

    Ajutes ESlint

--- src/createStudent.js
+++ src/createStudent.js
@@ -18,7 +18,7 @@
 const createStudent = (nome) => {
     const object = {
         name: nome,
-        feedback: () => 'Eita pessoa boa!'
+        feedback: () => 'Eita pessoa boa!',
     };
     return object;
 };
