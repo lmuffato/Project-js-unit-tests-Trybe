1fd2bada0e65a5d5ddf55cc4fccbfe259432c7ce
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-04-02 16:47:57 -0300

    lint corrigido novamente

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -23,7 +23,7 @@ const circle = require('../src/circle');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-// lógica da precisão vista no projeto da Débora Passos - https://github.com/tryber/sd-010-a-project-js-unit-tests/pull/148
+/* lógica da precisão vista no projeto da Débora Passos - https://github.com/tryber/sd-010-a-project-js-unit-tests/pull/148 */
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
     assert.strictEqual(typeof circle(1), 'object');
