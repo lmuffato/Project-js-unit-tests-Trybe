33e519764f45debef69eec8cebd88b831c3c6d57
pmaiamateus <pmaiamateus@gmail.com>
2021-04-02 11:13:27 -0300

    conserta average 2.0

--- tests/average.spec.js
+++ tests/average.spec.js
@@ -19,7 +19,7 @@ const average = require('../src/average');
 */
 function avg(array) {
   let sum = 0;
-  if (array[0] === undefined) {
+  if (array.length === 0) {
     return undefined;
   }
   for (let index = 0; index < array.length; index += 1) {
