a9dfd5bbe333dcc1cd3b38a5b961a70a338611af
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-04-02 16:49:47 -0300

    requisito 8 corrigido

--- src/myCounter.js
+++ src/myCounter.js
@@ -14,8 +14,8 @@ const myCounter = () => {
   let myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let newCounter = 2; newCounter <= 3; newCounter += 1) {
+      myArray.push(newCounter);
     }
   }
   return myArray;
