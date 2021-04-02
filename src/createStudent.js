86ff43743e6541fec5e0a936633750fc092040dc
Alan Albuquerque <alan.alb.flopes@gmail.com>
2021-04-02 05:01:55 -0400

    add createStudent.req[3]

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
-
+const createStudent = (nome) => {
+  const student = {
+    name: nome,
+    feedback: () =>{return 'Eita pessoa boa!'},
+  }
+  return student;
+};
 module.exports = createStudent;
