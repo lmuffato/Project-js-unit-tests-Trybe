2e382e3d43b3e80ceb7dd42da2b5884475d727f0
Alan Albuquerque <alan.alb.flopes@gmail.com>
2021-04-02 04:32:00 -0400

    add circle.req[

--- src/circle.js
+++ src/circle.js
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-
+const assert = require('assert');
 /*
   Essa função recebe o raio de um círculo,
   retornando um objeto contendo suas informações (Raio, Área e Circunferência).
@@ -27,5 +27,4 @@ const circle = (radius) => {
     circumference: 2 * PI * radius,
   };
 };
-
 module.exports = circle;
