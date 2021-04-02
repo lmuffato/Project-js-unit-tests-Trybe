8a4cacbee67eed5bacd38a816e033a40d2d14a49
Maria Lisboa <marialisboa1515@gmail.com>
2021-04-02 10:55:50 -0300

    requisito myCounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -11,14 +11,14 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  let myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counte = 2; counte <= 3; counte += 1) {
+      myArray.push(counte);
     }
   }
   return myArray;
 };
-
+console.log(myCounter());
 module.exports = myCounter;
