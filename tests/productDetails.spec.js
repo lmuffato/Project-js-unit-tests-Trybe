d8572e6476667a5a32a79edf591cddea8e1d16fb
Rodrigo Grande <rogrande0496@gmail.com>
2021-04-02 15:04:03 -0300

    fix requisito 7

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -35,7 +35,7 @@ describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
-    assert.strictEqual(typeof productDetails, 'array');
+    assert.ok(Array.isArray(productDetails('x', 'y')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(productDetails('x', 'y').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
