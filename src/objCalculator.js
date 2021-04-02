10ba57e8eef0dd94a5e78f68e30e74812520b06b
Alan Albuquerque <alan.alb.flopes@gmail.com>
2021-04-02 12:38:42 -0400

    add objCalculator.req[7]

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,10 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
-
+const calculator = {
+  add: (x, y) => x + y,
+  mult: (x, y) => Math.ceil(x * y),
+  div: (x, y) => Math.floor(x / y),
+  sub: (x, y) => x - y,
+};
 module.exports = calculator;
