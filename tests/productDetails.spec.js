a513d90f251ad700a9b5d4afb632bbf94b035c59
johnpligeiro <johnpligeiro@gmail.com>
2021-04-02 20:54:00 -0300

    Correção do requisito product details

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -35,7 +35,7 @@ describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
-    assert.strictEqual(typeof productDetails('a', 'b'), 'array');
+    assert.deepStrictEqual(typeof productDetails('a', 'b'), 'array');
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(productDetails('a', 'b').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
