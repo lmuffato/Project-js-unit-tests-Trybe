e5eb452e85b118eecd0d2208db18f913b591a5d6
Rodrigo Grande <rogrande0496@gmail.com>
2021-04-02 14:25:58 -0300

    fix requisito 8

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -20,7 +20,7 @@
 */
 const add = (a, b) => a + b;
 const mult = (a, b) => a * b;
-const div = (a, b) => a / b;
+const div = (a, b) => Math.floor(a / b);
 const sub = (a, b) => a - b;
 
 const calculator = {
