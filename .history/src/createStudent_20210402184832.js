b99b4cc47dca7c7cfee625277c134c20d0a234f4
Juan Pablo Martins <juanpm@pop-os.localdomain>
2021-04-03 00:30:24 -0300

    requisito creatSutendt feito eu acho

--- .history/src/createStudent_20210402184832.js
+++ .history/src/createStudent_20210402184832.js
@@ -15,14 +15,6 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {
-  const createStudent = (name) => {
-    const feedback = () => 'Eita pessoa boa!'
-    const student = {
-      name,
-      feedback
-    };
-    return student;
-};
+const createStudent = () => {};
 
 module.exports = createStudent;
