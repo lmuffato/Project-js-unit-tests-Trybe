851b111b53aa6643d05adc772deba7bb3e44b469
GetulioGutemberg <getulio.dev@gmail.com>
2021-04-03 12:11:23 -0300

    resolve requisito 5

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -30,7 +30,7 @@ describe('#circle', () => {
     assert.strictEqual(circle(), undefined); // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.strictEqual(circle(2).circumference, '13'); // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.strictEqual(circle(3).area, '28'); // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    //assert.strictEqual(circle(3),  {radius: 3, area: '28', circumference: '19'});   
+    // assert.strictEqual(circle(3),  {radius: 3, area: '28', circumference: '19'});
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
   });
 });
