60a8cc3cf82d6a620602043891b2ad130b09b958
johnpligeiro <johnpligeiro@gmail.com>
2021-04-03 00:19:12 -0300

    restaurante continuação

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -97,7 +97,6 @@ describe('#createMenu', () => {
     objetoRetornado.order('coxinha');
     // Retorno: ["coxinha"]
     assert.strictEqual(consumptionArr.includes('coxinha'), true);
-    consumptionArr = [];
     // Agora faça o PASSO 3 no arquivo `src/restaurant.js`.
     // --------------------------------------------------------------------------------------
     // TESTE 6: Verifique que as três orders seguintes, de bebidas e comidas mescladas, somam três itens no array `objetoRetornado.consumption` conforme os itens pedidos.
@@ -108,7 +107,7 @@ describe('#createMenu', () => {
     // objetoRetornado.order("sashimi");
     // objetoRetornado.consumption // Retorno: ["coxinha", "agua", "sopa", "sashimi"]
     // ```
-    consumptionArr = [];
+    consumptionArr = [0];
     objetoRetornado.order('coxinha');
     objetoRetornado.order('agua');
     objetoRetornado.order('sopa');
