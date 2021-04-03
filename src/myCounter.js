8ea3965362bfd2088a373716b002e8f48d253486
Luiz Putinatti <luiz_edu16@hotmail.com>
2021-04-03 08:57:55 -0300

    ajuste lint requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -14,7 +14,6 @@
   Comportamento:
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
-
 const myCounter = () => {
   let myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
@@ -23,9 +22,7 @@ const myCounter = () => {
       myArray.push(counter);
     }
   }
-  return myArray;
+return myArray;
 };
-console.log(myCounter());
-
 
 module.exports = myCounter;
