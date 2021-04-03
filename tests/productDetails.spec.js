9a7a59f36c5df5fb281683eaee9726d06b6982f3
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-04-02 21:57:36 -0300

    Requisito 7 - corrige

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,7 +33,7 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail()
+    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
     // Teste que o array retornado pela função contém dois itens dentro.
