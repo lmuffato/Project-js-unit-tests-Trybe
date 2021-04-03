f1fa3d3e89ff87d0f6068d804dba2a5dbd21da06
renancosta21 <renancosta21@gmail.com>
2021-04-03 12:11:44 -0300

    requisito 1

--- src/average.js
+++ src/average.js
@@ -12,8 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // implementar o código aqui
+const average = (arrayElements) => {
+  let calculus = 0;
+  if (arrayElements.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < arrayElements.length; index += 1) {
+    if (typeof arrayElements[index] === 'number') {
+      calculus += arrayElements[index];
+    } else {
+      return undefined;
+    }
+  }
+  return Math.round(calculus / arrayElements.length);
 };
 
 module.exports = average;
