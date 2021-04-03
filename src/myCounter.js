6e12d898a4a80048a96fbf51d147c18f60e24e79
Luiz Putinatti <luiz_edu16@hotmail.com>
2021-04-03 09:13:07 -0300

    ajuste lint requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -12,9 +12,10 @@
   - Nenhum.
 
   Comportamento:
-  myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];*/
+  myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
+*/
 const myCounter = () => {
-  let myArray = [];
+  const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (let counter = 2; counter <= 3; counter += 1) {
