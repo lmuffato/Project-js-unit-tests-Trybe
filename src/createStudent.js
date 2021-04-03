2df506a749b6c4a8f4dfc4614729df341a37e115
Roberto Natividade <robertonnv@gmail.com>
2021-04-02 23:18:44 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  
+  const mensagem = 'Eita pessoa boa!';
+  const student = {
+    name: nome,
+    feedback: () => mensagem,
+  };
+  return student;
+};
 
 module.exports = createStudent;
