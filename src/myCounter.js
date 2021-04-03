0aaee11be4f47f15f4c69afb9ef2f0bb2d61c6dd
GetulioGutemberg <getulio.dev@gmail.com>
2021-04-03 13:37:45 -0300

    resolve requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -14,7 +14,7 @@ const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
+    for (let counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
   }
@@ -22,3 +22,5 @@ const myCounter = () => {
 };
 
 module.exports = myCounter;
+
+console.log(myCounter());
--- src/objCalculator.js
+++ src/objCalculator.js
@@ -23,7 +23,7 @@ const calculator = {
   add: (a, b) => (a + b),
   mult: (a, b) => (a * b),
   div: (a, b) => Math.floor(a / b),
-  sub: (a, b) => (a - b)
+  sub: (a, b) => (a - b),
 };
 
 module.exports = calculator;
