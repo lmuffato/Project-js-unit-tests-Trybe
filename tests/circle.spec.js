5710de47e82225fc86e265fcfff13e8d010d765f
Guilherme Sucupira Oliveira <guilherme.so23@gmail.com>
2021-04-03 13:52:25 -0300

    Concluido exercicio Numbers

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -23,17 +23,17 @@ const circle = require('../src/circle');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-const describe = ('#circle', () => {
+describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
     assert.fail();
     assert.strictEqual(typeof circle(1), 'object');
     assert.strictEqual(Object.keys(circle(1)).length, 3);
     assert.strictEqual(circle(), undefined);
     assert.strictEqual(circle(2).circumference, 12.56);
-    assert.strictEqual(circle(3).area, 28.259999999999998);
+    assert.strictEqual(circle(3).area, 28.26);
     assert.deepStrictEqual(circle(3), {
       radius: 3,
-      area: 28.259999999999998,
+      area: 28.26,
       circumference: 18.84,
     });
     // ESCREVA SEUS TESTES ABAIXO:
