e9b2441466b8522ec04c7f8e2f5484c3afbbec20
Luiz Otavio <luiz.otavio273@gmail.com>
2021-04-03 11:17:11 -0300

    corrigindo exercicio circle quarta tentativa

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,11 +25,11 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.strictEqual(typeof (circle(1), 'object'));
+    assert.strictEqual(typeof circle(1), 'object');
     assert.strictEqual(Object.entries(circle(3)).length, 3);
     assert.strictEqual(circle(), undefined);
     assert.strictEqual((circle(2)).circumference, (2 * 3.14 * 2));
-    assert.strictEqual(parseFloat(circle(3).area.toPrecision(2)), (3.14 * 3 * 3).toPrecision(2));
-    assert.strictEqual(circle(3), { radius: 3, area: 3.14 * 3 * 3, circumference: 2 * 3.14 * 3 });
+    assert.strictEqual(circle(3).area.toPrecision(2), (3.14 * 3 * 3).toPrecision(2));
+    assert.deepStrictEqual(circle(3), { radius: 3, area: 3.14 * 3 * 3, circumference: 2 * 3.14 * 3 });
   });
 });
