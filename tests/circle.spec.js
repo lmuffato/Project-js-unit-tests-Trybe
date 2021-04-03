1799ffec7f752e77f6a5ffe87f0cb305a1b43d17
Luiz Putinatti <luiz_edu16@hotmail.com>
2021-04-02 23:15:45 -0300

    ajuste lint

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,7 +25,6 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
     // Teste se o objeto retornado tem 3 entradas.
@@ -34,7 +33,7 @@ describe('#circle', () => {
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     const circle3 = { radius: 3, area: 28.259999999999998, circumference: 18.84 };
-    assert.strictEqual(typeof(circle(1)), null);
+    assert.strictEqual(typeof (circle(1)), null);
     assert.strictEqual(circle(1).length, 3);
     assert.strictEqual(circle(), undefined);
     assert.strictEqual(circle(2).circumference, 12,56);
