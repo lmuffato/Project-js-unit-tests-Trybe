f0bb2af015891066e1a00d149ed9d15f282c1c9f
Luiz Otavio <luiz.otavio273@gmail.com>
2021-04-03 11:22:24 -0300

    Eslint

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -30,7 +30,7 @@ describe('#circle', () => {
     assert.strictEqual(circle(), undefined);
     assert.strictEqual((circle(2)).circumference, (2 * 3.14 * 2));
     assert.strictEqual(circle(3).area.toPrecision(2), (3.14 * 3 * 3).toPrecision(2));
-    assert.deepStrictEqual(circle(3), { 
-    radius: 3, area: 3.14 * 3 * 3, circumference: 2 * 3.14 * 3 });
+    assert.deepStrictEqual(circle(3), {
+      radius: 3, area: 3.14 * 3 * 3, circumference: 2 * 3.14 * 3 });
   });
 });
