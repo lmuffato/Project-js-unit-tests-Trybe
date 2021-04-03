0d50e3afa30983351fefec62b03b4016f35a824f
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-04-03 09:00:45 -0300

    requisito 4, reparo no segundo e quinto

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -29,13 +29,13 @@ describe('#circle', () => {
     // Teste se circle retorna um objeto.
     assert.deepStrictEqual(typeof circle(1), 'object');
     // Teste se o objeto retornado tem 3 entradas.
-    assert.deepStrictEqual(Object.entries(circle(1).length), 3);
+    assert.deepStrictEqual(Object.keys(circle(1)).length, 3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.deepStrictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.deepStrictEqual(circle(2).circumference, 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.deepStrictEqual(circle(3).area, 28.259999999999998);
+    assert.deepStrictEqual(circle(3).area.toFixed(2), 28.25);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     const circle3 = circle(3);
     circle3.area = parseFloat((circle3.area).toFixed(2));
