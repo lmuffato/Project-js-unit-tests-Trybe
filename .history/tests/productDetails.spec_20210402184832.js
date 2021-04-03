f1e155af0598e565967ce85528c441a524755ff3
Juan Pablo Martins <juanpm@pop-os.localdomain>
2021-04-03 00:57:53 -0300

    requisito productDetails espero que certo

--- .history/tests/productDetails.spec_20210402184832.js
+++ .history/tests/productDetails.spec_20210402184832.js
@@ -35,22 +35,10 @@ describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
     assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
-    
-    assert.ok(Array.isArray(productDetails()));
     // Teste que o retorno da função é um array.
-
-    assert.strictEqual(productDetails().length, 2);
     // Teste que o array retornado pela função contém dois itens dentro.
-
-    assert.ok(productDetails().every((item) => typeof item === 'object'));
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-
-    const test = productDetails('product1', 'product2');
-    assert.strictEqual(typeof test[0] === 'object' && typeof test[1] === 'object', true);
     // Teste que os dois objetos são diferentes entre si.
-
-    assert.strictEqual(test[0].details.productId.includes('123')
-    && test[1].details.productId.includes('123'), true);
     // (Difícil) Teste que os dois productIds terminam com 123.
   });
 });
