de75dfb1886845e61ee2c522878ffe8930ff3cf7
renancosta21 <renancosta21@gmail.com>
2021-04-03 12:29:45 -0300

    requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-unused-vars */
 
 const assert = require('assert');
+const { isArray } = require('util');
 const productDetails = require('../src/productDetails');
 
 /*
@@ -33,12 +34,19 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    const details = productDetails('porta', 'livro');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(details), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(details.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof details[0], 'object');
+    assert.strictEqual(typeof details[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.strictEqual(details[0] === details[1], false);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(details[0].details.productId.endsWith('123'), true);
+    assert.strictEqual(details[1].details.productId.endsWith('123'), true);
   });
 });
