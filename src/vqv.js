298c62355a1daaf7588c63ab00243053b7fb4857
Luiz Putinatti <luiz_edu16@hotmail.com>
2021-04-02 22:40:09 -0300

    ajuste requsito 3

--- src/vqv.js
+++ src/vqv.js
@@ -17,7 +17,7 @@
 */
 
 const vqv = (nome, idade) => {
-  if (nome === ' ' || idade === ' ') {
+  if (typeof (nome) !== 'string' || typeof (idade) !== 'number') {
     return undefined;
   }
   return `Oi, meu nome é ${nome}!
@@ -25,4 +25,5 @@ const vqv = (nome, idade) => {
   trabalho na Trybe e mando muito em programação!
   #VQV!`;
 };
+console.log(vqv());
 module.exports = vqv;
