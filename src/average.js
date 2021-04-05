a7d5a5b0cfc5fe7210a88fab8140a34e010691d9
diogomarin <diogoamarin@gmail.com>
2021-04-05 11:01:09 -0300

    Initial commit

--- src/average.js
+++ src/average.js
@@ -12,6 +12,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let sum = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof (array[i]) === 'number') {
+      sum += array[i];
+    } else {
+      return undefined;
+    }
+  }
+  return Math.round(sum / array.length);
+};
 
 module.exports = average;
