01eb1d38ac65ff6d884beaf2e9b7abceed7832a4
jlfagundes <jlfagundes@gmail.com>
2021-04-05 11:44:58 -0300

    Requirement createStudent done

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const students = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return students;
+};
 
 module.exports = createStudent;
