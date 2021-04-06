a35784488f1ce6d0b23811e0bc308f94b471744a
michael-petterson-06 <michaelpetterson06l@gmailo.com>
2021-04-06 12:20:18 -0300

    average4

--- src/average.js
+++ src/average.js
@@ -16,18 +16,17 @@ const average = (array) => {
   let total = 0;
   let tamanho = array.length;
   let media = 0;
-    for (let i = 0; i < array.length; i+= 1) {
+  for (let i = 0; i < array.length; i += 1) {
     if (typeof (array[i]) !== 'number') {
       media = 'undefined';
       return media;
-    } else {
-      total += array[i];}
+    } else {total += array[i]; }
   }
   media = Math.round(total / tamanho);
   return media;
 };
 const array = [1, 2.2, null, 4, 5.8];
-const chamarFunction = (average(array))
+const chamarFunction = (average(array));
 console.log(chamarFunction);
 
 module.exports = average;
