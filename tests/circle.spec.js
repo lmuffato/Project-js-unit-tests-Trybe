a53efa578f6bef76454f0e5495f26fd64ed61d2e
Stanrley Barbosa <3011stan@gmail.com>
2021-04-06 20:21:04 -0300

    Sem ideias ja

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -35,7 +35,7 @@ describe('#circle', () => {
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.strictEqual(circle([2]).circumference, 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.strictEqual(circle([3]).area, 28.26);
+    assert.strictEqual(parseFloat(circle([3]).area.toFixed(2)), 28.26);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     const objectCompare = { radius: 3, area: 28.26, circumference: 18.84 };
     assert.deepStrictEqual(circle([3]), objectCompare);
