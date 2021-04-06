8a7fbacb3c1e4956c8ce3a6159b56c6b47a1fe24
Stanrley Barbosa <3011stan@gmail.com>
2021-04-06 20:05:56 -0300

    Erro linha muito grande

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -35,7 +35,8 @@ describe('#circle', () => {
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.strictEqual(circle([2]).circumference, 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.strictEqual(parseFloat(circle([3]).area.toPrecision(4)), parseFloat((3.14 * 3 * 3).toPrecision(4)));
+    const compare = parseFloat((3.14 * 3 * 3).toPrecision(4));
+    assert.strictEqual(parseFloat(circle([3]).area.toPrecision(4)), compare);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     const objectCompare = { radius: 3, area: 28.26, circumference: 18.84 };
     assert.deepStrictEqual(circle([3]), objectCompare);
