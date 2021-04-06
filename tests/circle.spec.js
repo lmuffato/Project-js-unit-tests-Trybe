bdd3db449324ac347836f202279a7d10067c963a
Stanrley Barbosa <3011stan@gmail.com>
2021-04-06 17:49:10 -0300

    Problema 2 casas decimais

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -20,7 +20,7 @@ const circle = require('../src/circle');
         Uma solução pra isso pode ser fazer a soma no seguinte formato: `parseFloat((0.2 + 0.1).toPrecision(2))`.
         Use esse conhecimento para te ajudar a lidar com possíveis problemas que esses testes trarão!
 
-  OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
+  OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente!
 */
 
 describe('#circle', () => {
