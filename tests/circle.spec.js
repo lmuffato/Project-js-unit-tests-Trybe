f1327cb52675f02d6e19af05ee5eababb75ac797
Stanrley Barbosa <3011stan@gmail.com>
2021-04-06 19:56:44 -0300

    Resolvendo bug parseFloat

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -35,7 +35,7 @@ describe('#circle', () => {
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     assert.strictEqual(circle([2]).circumference, 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.strictEqual(parseFloat(circle([3]).area), 28.26);
+    assert.strictEqual(circle([3]).area, parseFloat((3.14 * 3 * 3).toPrecision(4)));
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
     const objectCompare = { radius: 3, area: 28.26, circumference: 18.84 };
     assert.deepStrictEqual(circle([3]), objectCompare);
