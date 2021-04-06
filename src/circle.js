d9bfa65bedba6c5c74ce3720d194ec1be61edff1
Stanrley Barbosa <3011stan@gmail.com>
2021-04-06 20:10:30 -0300

    Problem avaliator

--- src/circle.js
+++ src/circle.js
@@ -23,7 +23,7 @@ const circle = (radius) => {
   if (!radius) { return undefined; }
   return {
     radius: radius[0],
-    area: parseFloat((PI * radius * radius).toPrecision(4)),
+    area: parseFloat((PI * radius * radius).toFixed(2)),
     circumference: 2 * PI * radius,
   };
 };
