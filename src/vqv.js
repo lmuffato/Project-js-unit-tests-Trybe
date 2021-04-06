506d4797d50d9998e4066c48c76bd3396ab45011
michael-petterson-06 <michaelpetterson06l@gmailo.com>
2021-04-06 15:14:03 -0300

    vqv3

--- src/vqv.js
+++ src/vqv.js
@@ -22,10 +22,8 @@ const vqv = (nome, idade) => {
   if (parametroNome === undefined && parametroIdade === undefined) {
     return parametroIdade;
   }
-  return `Oi, meu nome é ${nome}!
-  tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+  return `Oi, meu nome é ${nome}!\ntenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
 };
+console.log(vqv('Tunico', 29));
 
 module.exports = vqv;
