92af71ddf3cc8a9fa38b10720880137e247ed5f7
Eicke Yumoto <eickeyumoto@gmail.com>
2021-04-07 19:07:08 -0300

    correção lint***

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,17 +33,19 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    const funcao = productDetails('Alcool gel', 'Máscara')
+    const funcao = productDetails('Alcool gel', 'Máscara');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
-    assert.strictEqual(typeof funcao, 'array')
+    assert.strictEqual(typeof funcao, 'array');
     // Teste que o array retornado pela função contém dois itens dentro.
-    assert.strictEqual(funcao.length, 2)
+    assert.strictEqual(funcao.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    assert.strictEqual(typeof Object.keys(funcao), 'object')
+    assert.strictEqual(typeof Object.keys(funcao), 'object');
     // Teste que os dois objetos são diferentes entre si.
-    assert.notStrictEqual(Object.entries(funcao[0]), Object.entries(funcao[1]))
+    assert.notStrictEqual(Object.entries(funcao[0]), Object.entries(funcao[1]));
     // (Difícil) Teste que os dois productIds terminam com 123.
-    assert.ok(productDetails(funcao[0].details.productId.endsWith('123') && funcao[1].details.productId.endsWith('123'), true))
+    const func1 = funcao[0].details.productId.endsWith('123');
+    const func2 = funcao[1].details.productId.endsWith('123');
+    assert.ok(productDetails(func1 && func2, true));
   });
 });
