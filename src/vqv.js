c50660377f8cc36191f2b5981fafa54dffcc5a2d
Junior Portela <portelajr14@gmail.com>
2021-04-07 20:33:49 -0300

    Reestruturação função vqv e ajustes de lint

--- src/vqv.js
+++ src/vqv.js
@@ -15,16 +15,15 @@
       trabalho na Trybe e mando muito em programação!
       #VQV!'
 */
-
 const vqv = (nome, idade) => {
-  if (typeof nome === 'string' && typeof idade === 'number') {
-    const text = `${'Oi, meu nome é '+ nome + '!\n'
-    +'Tenho ' + idade + ' anos,\n'
-    +'trabalho na Trybe e mando muito em programação!\n'
-    +'#VQV!'}`
-    return text;
+  if (nome === undefined && idade === undefined) {
+    return undefined;
   }
-  return undefined;
+
+  return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 
 module.exports = vqv;
