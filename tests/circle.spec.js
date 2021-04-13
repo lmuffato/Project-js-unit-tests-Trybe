6ef81b257b1fbf880d639f28ec10ecd1e311ada1
Eicke Yumoto <eickeyumoto@gmail.com>
2021-04-13 10:55:33 -0300

    lint

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -33,10 +33,11 @@ describe('#circle', () => {
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    const test3 = circle(3);
     assert.strictEqual(round(circle(2).circumference, 2), 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    assert.strictEqual(round(circle(3).area, 2), 28.26);
+    assert.strictEqual(round(test3.area, 2), 28.26);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    assert.deepStrictEqual(circle(3), { radius: 3, area: 28.259999999999998, circumference: 18.84 });
+    assert.deepStrictEqual(test3, { radius: 3, area: 28.259999999999998, circumference: 18.84 });
   });
 });
