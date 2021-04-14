5f6aaff13577c3c0c7280aff9b805088533f5554
Adriel Falcao Telito <adriel.telito@pampa.compasso>
2021-04-13 22:41:46 -0300

    subindo dados

--- src/average.js
+++ src/average.js
@@ -11,7 +11,7 @@
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
-const average = (array) => {
+const average = (array) => { 
   var soma = 0
   for(var i = 0; i < array.length; i++){
     if(typeof array[i] != 'number'){
