2a3ed918ad5e08128087e71b37ad7e23e341e0f5
Adriel Falcao Telito <adriel.telito@pampa.compasso>
2021-04-15 17:45:33 -0300

    enviando exercicio 4

--- tests/myCounter.spec.js
+++ tests/myCounter.spec.js
@@ -17,7 +17,7 @@ const myCounter = require('../src/myCounter');
 
 describe('#myCounter', () => {
   it('guarantees the function behaves as expected', () => {
-    const expectedOutput = [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
+    const expectedOutput = [0, 2, 3];
     assert.deepStrictEqual(myCounter(), expectedOutput);
   });
 });
