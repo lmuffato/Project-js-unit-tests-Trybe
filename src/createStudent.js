69d59dbddd41201ff9c36e8c3ddef0718fb54334
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2021-04-15 14:30:17 -0300

    Requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const returnFeedback = () => 'Eita pessoa boa!';
+
+const createStudent = (student) => ({
+  name: student,
+  feedback: returnFeedback,
+});
 
 module.exports = createStudent;
