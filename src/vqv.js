1ab753ceee9bf723420955fc3bbe29ab04f3dc21
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2021-04-15 16:57:02 -0300

    Refatoração vqv.js

--- src/vqv.js
+++ src/vqv.js
@@ -17,12 +17,11 @@
 */
 
 const vqv = (nome, idade) => {
-  if (nome && idade) {
-    return `Oi, meu nome é ${nome}!
+  if (!nome && !idade) return undefined;
+  return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
-  } return undefined;
 };
 
 module.exports = vqv;
