cbd0348ebb887b591b50654c6339deace4eaf9c4
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2021-04-15 14:51:02 -0300

    Requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,18 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
+    const func = productDetails('Alcool gel', 'Máscara');
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(func), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(func.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof func[0] && typeof func[1], 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notStrictEqual(func[0], func[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(func[0].details.productId, 'Alcool gel123');
+    assert.strictEqual(func[1].details.productId, 'Máscara123');
   });
 });
