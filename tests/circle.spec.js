b795f309d27909146df8b6868d9d71c8fbfcb087
Adriel Falcao Telito <adriel.telito@pampa.compasso>
2021-04-15 15:38:02 -0300

    subindo segundo exercicio

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,7 +25,6 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
     // Teste se o objeto retornado tem 3 entradas.
@@ -33,5 +32,9 @@ describe('#circle', () => {
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert.strictEqual(circle(), undefined);
+    assert.strictEqual(circle(1), [1,3.14,6.28]);
+    assert.strictEqual(circle(7), [7,153.86,43.96]);
+    assert.strictEqual(circle(3), [3,28,26,18,84]);
   });
 });
