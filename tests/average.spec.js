79aa4bbf216564a509693cbfb7b4c2d6ff6169fe
pmaiamateus <pmaiamateus@gmail.com>
2021-04-18 18:11:05 -0300

    reenviando average

--- tests/average.spec.js
+++ tests/average.spec.js
@@ -16,19 +16,21 @@ const assert = require('assert');
 
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
+
 const average = (array) => {
   let sum = 0;
-  if (array[0] === undefined) {
-    return;
+  if (array.length === 0) {
+    return undefined;
   }
   for (let index = 0; index < array.length; index += 1) {
     if (typeof (array[index]) !== 'number') {
-      return;
+      return undefined;
     }
     sum += array[index];
   }
   return Math.round(sum / array.length);
 };
+
 module.exports = average;
 
 describe('#average', () => {
