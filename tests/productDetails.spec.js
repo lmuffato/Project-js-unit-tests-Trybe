d0f9f93a4187b0afd0a762b0bee8267629e0d8ec
Junior Portela <portelajr14@gmail.com>
2021-04-20 01:48:15 -0300

    Requisito 6: implementados testes para a funcao productDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,19 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    const products = productDetails('arroz', 'feijao');
+    assert.ok(Array.isArray(products));
+    assert.strictEqual(products.length, 2);
+    assert.strictEqual(typeof Object.values(products), 'object');
+    assert.notDeepStrictEqual(products[0], products[1]);
+    const obj1 = products[0].details.productId;
+    const obj2 = products[1].details.productId;
+    assert.strictEqual(obj1.endsWith(123), obj2.endsWith(123));
     // ESCREVA SEUS TESTES ABAIXO:
-    // Teste que o retorno da função é um array.
-    // Teste que o array retornado pela função contém dois itens dentro.
-    // Teste que os dois itens dentro do array retornado pela função são objetos.
-    // Teste que os dois objetos são diferentes entre si.
-    // (Difícil) Teste que os dois productIds terminam com 123.
+    // Teste que o retorno da função é um array. DONE
+    // Teste que o array retornado pela função contém dois itens dentro. DONE
+    // Teste que os dois itens dentro do array retornado pela função são objetos. DONE
+    // Teste que os dois objetos são diferentes entre si. DONE
+    // (Difícil) Teste que os dois productIds terminam com 123. DONE
   });
 });
