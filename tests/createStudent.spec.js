96d51ecbb8ab989d2686fb1ec29d296ee33ecb76
pmaiamateus <pmaiamateus@gmail.com>
2021-04-20 12:10:39 -0300

    envia createStudent

--- tests/createStudent.spec.js
+++ tests/createStudent.spec.js
@@ -1,8 +1,4 @@
 /* eslint-disable max-len */
-
-const assert = require('assert');
-const createStudent = require('../src/createStudent');
-
 /*
   Dada uma função chamada createStudent que recebe como parâmetro um nome, retorne um objeto que contenha duas chaves:
     (1) name, contendo o nome passado como parâmetro;
@@ -20,24 +16,9 @@ const createStudent = require('../src/createStudent');
 
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
-const message = 'Eita pessoa boa!';
-
-describe('#createStudent', () => {
-  it('returns the object as specified', () => {
-    const estudante = createStudent('Leandrão, o Lobo Solitário');
-    assert.strictEqual(typeof estudante, 'object');
-    assert.strictEqual(typeof estudante.feedback, 'function');
-    assert.strictEqual(estudante.name, 'Leandrão, o Lobo Solitário');
-    assert.strictEqual(estudante.feedback(), message);
-    const estudante2 = createStudent('Nobre');
-    assert.strictEqual(typeof estudante2, 'object');
-    assert.strictEqual(typeof estudante2.feedback, 'function');
-    assert.strictEqual(estudante2.name, 'Nobre');
-    assert.strictEqual(estudante2.feedback(), message);
-    const estudante3 = createStudent('Inácio');
-    assert.strictEqual(typeof estudante3, 'object');
-    assert.strictEqual(typeof estudante3.feedback, 'function');
-    assert.strictEqual(estudante3.name, 'Inácio');
-    assert.strictEqual(estudante3.feedback(), message);
-  });
-});
+const createStudent = (studentName) => {
+  let student = { name: studentName,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return student;
+};
