538c91990560d9f22fab8384c41e725e274e397f
pmaiamateus <pmaiamateus@gmail.com>
2021-04-20 12:22:22 -0300

    conserta createStudent 3.0

--- tests/createStudent.spec.js
+++ tests/createStudent.spec.js
@@ -1,8 +1,5 @@
 /* eslint-disable max-len */
 
-const assert = require('assert');
-const createStudent = require('../src/createStudent');
-
 /*
   Dada uma função chamada createStudent que recebe como parâmetro um nome, retorne um objeto que contenha duas chaves:
     (1) name, contendo o nome passado como parâmetro;
@@ -26,23 +23,3 @@ const createStudent = (studentName) => {
   };
   return student;
 };
-
-describe('#createStudent', () => {
-  it('returns the object as specified', () => {
-    const estudante = createStudent('Leandrão, o Lobo Solitário');
-    assert.strictEqual(typeof estudante, 'object');
-    assert.strictEqual(typeof estudante.feedback, 'function');
-    assert.strictEqual(estudante.name, 'Leandrão, o Lobo Solitário');
-    assert.strictEqual(estudante.feedback(), message);
-    const estudante2 = createStudent('Nobre');
-    assert.strictEqual(typeof estudante2, 'object');
-    assert.strictEqual(typeof estudante2.feedback, 'function');
-    assert.strictEqual(estudante2.name, 'Nobre');
-    assert.strictEqual(estudante2.feedback(), message);
-    const estudante3 = createStudent('Inácio');
-    assert.strictEqual(typeof estudante3, 'object');
-    assert.strictEqual(typeof estudante3.feedback, 'function');
-    assert.strictEqual(estudante3.name, 'Inácio');
-    assert.strictEqual(estudante3.feedback(), message);
-  });
-});
