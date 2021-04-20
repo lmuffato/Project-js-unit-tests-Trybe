44cb397ccad3c57cd767e0b03bf19bffe42ff15e
pmaiamateus <pmaiamateus@gmail.com>
2021-04-20 16:17:21 -0300

    adiciona vqv

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (nome === undefined || idade === undefined) {
+    return;
+  }
+  let phrase = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+  return phrase;
+};
 
 module.exports = vqv;
