5d11b605d0617052df7e49e11f7478605b728b5b
pmaiamateus <pmaiamateus@gmail.com>
2021-04-20 15:56:22 -0300

    conserta lint de myCounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -12,10 +12,10 @@
 
 const myCounter = () => {
   let myArray = [];
-  for (let counter = 0; counter <= 3; counter += 1) {
+  for (let counter1 = 0; counter1 <= 3; counter1 += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
+      myArray.push(counter2);
     }
   }
   return myArray;
