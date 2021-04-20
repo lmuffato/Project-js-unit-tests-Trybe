f6cd557da98854a3dc1f7f292bea2bcf59631ebf
pmaiamateus <pmaiamateus@gmail.com>
2021-04-20 15:45:01 -0300

    conserta productDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -35,32 +35,34 @@ describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
-    assert.deepStrictEqual(typeof (productDetails('Ácool gel', 'Máscara')), 'object');
+    const parameter1 = 'Álcool gel';
+    const parameter2 = 'Máscara';
+    assert.deepStrictEqual(typeof (productDetails(parameter1, parameter2)), 'object');
     // Teste que o array retornado pela função contém dois itens dentro.
-    assert.deepStrictEqual(Object.entries(productDetails('Ácool gel', 'Máscara')).length, 2);
+    assert.deepStrictEqual(Object.entries(productDetails(parameter1, parameter2)).length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    const object1 = Object.entries(productDetails('Alcool gel', 'Máscara')[0]);
-    const object2 = Object.entries(productDetails('Alcool gel', 'Máscara')[1]);
+    const object1 = Object.entries(productDetails(parameter1, parameter2)[0]);
+    const object2 = Object.entries(productDetails(parameter1, parameter2)[1]);
     assert.deepStrictEqual(typeof (object1), 'object');
     assert.deepStrictEqual(typeof (object2), 'object');
     // Teste que os dois objetos são diferentes entre si.
     assert.notDeepStrictEqual(object1, object2);
     // (Difícil) Teste que os dois productIds terminam com 123.
-    let products = productDetails('Alcool gel', 'Máscara')
-    let productcode1 = products[0].details.productId
-    let productcode2 = products[1].details.productId
+    let products = productDetails(parameter1, parameter2);
+    let productcode1 = products[0].details.productId;
+    let productcode2 = products[1].details.productId;
     const getNumber = (code) => {
-      let splitter = code.split('')
-      let numbers = []
-      for (index = code.length - 3; index < code.length; index ++){
-        numbers.push(splitter[index])
+      let splitter = code.split('');
+      let numbers = [];
+      for (let index = code.length - 3; index < code.length; index += 1) {
+        numbers.push(splitter[index]);
       }
-      let numbersJoined = numbers.join('')
-      return numbersJoined
-    }
-    const numbersProduct1 = getNumber(productcode1)
-    const numbersProduct2 = getNumber(productcode2)
-    assert.deepStrictEqual(numbersProduct1, '123')
-    assert.deepStrictEqual(numbersProduct2, '123')
+      let numbersJoined = numbers.join('');
+      return numbersJoined;
+    };
+    const numbersProduct1 = getNumber(productcode1);
+    const numbersProduct2 = getNumber(productcode2);
+    assert.deepStrictEqual(numbersProduct1, '123');
+    assert.deepStrictEqual(numbersProduct2, '123');
   });
 });
