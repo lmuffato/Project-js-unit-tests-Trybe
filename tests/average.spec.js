5944526c94a718449c0d1a7eff78b3771c8c780a
pmaiamateus <pmaiamateus@gmail.com>
2021-04-20 12:30:23 -0300

    tentando usar de novo npm

--- tests/average.spec.js
+++ tests/average.spec.js
@@ -1,5 +1,7 @@
 /* eslint-disable max-len */
 
+const assert = require('assert');
+
 /*
   A função average recebe um array (tamanho variável) e retorna a média dos valores recebidos.
   Caso a função receba algum valor não númerico ou um array vazio, o valor undefined deve ser retornado.
@@ -30,3 +32,23 @@ const average = (array) => {
 };
 
 module.exports = average;
+
+describe('#average', () => {
+  it('tests function average\'s behaviour as specified', () => {
+    assert.strictEqual(average([3, 4, 5]), 4);
+    assert.strictEqual(average([1, 2, 3, '4', 5]), undefined);
+    assert.strictEqual(average([0, 0, 0, 0, 0, 0, 0]), 0);
+    assert.strictEqual(average([1, 2, '3']), undefined);
+    assert.strictEqual(average([1, 2, 3]), 2);
+    assert.strictEqual(average([0, 0, 0, 0, 0, 0, 1]), 0);
+
+    assert.strictEqual(average([]), undefined);
+    assert.strictEqual(average([' ']), undefined);
+    assert.strictEqual(average(['um', 'dois', 'tres']), undefined);
+    assert.strictEqual(average([47, 63, 122]), 77);
+
+    assert.strictEqual(average([-11, 2, 5]), -1);
+
+    assert.strictEqual(average([-11, -5, 2]), -5);
+  });
+});
