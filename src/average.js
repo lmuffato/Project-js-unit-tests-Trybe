f056794053c41db2266b44f81ab2e85bbf28acfd
akirakiraa <andretaki2@gmail.com>
2021-04-22 00:04:36 -0300

    Round e Eslint

--- src/average.js
+++ src/average.js
@@ -11,18 +11,21 @@
     - average([1, 1]) // Retorno: 1;
     - average([1, '2']) // Retorno: undefined;
 */
-const average = (numArray) => {};
-let result = 0;
-if (numArray.length < 1) {
-  return undefined;
-}
-for (let index = 0; index < numArray.length; index += 1) {
-  if (typeof numArray[index] !== "number") {
+const average = (numArray) => {
+  // add your implementation here
+  let result = 0;
+  if (numArray.length < 1) {
     return undefined;
   }
-}
-for (let index2 = 0; index2 < numArray.length; index2 += 1) {
-  result += numArray[index2];
-}
-return result;
+  for (let index = 0; index < numArray.length; index += 1) {
+    if (typeof numArray[index] !== 'number') {
+      return undefined;
+    }
+  }
+  for (let index2 = 0; index2 < numArray.length; index2 += 1) {
+    result += numArray[index2];
+  }
+  result = Math.round(result / numArray.length);
+  return result;
+};
 module.exports = average;
