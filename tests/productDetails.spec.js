4b25f3ef327a3ef0e4ed99333620dbb8aa57d4a6
Victor Ricco <victor.ricco001@gmail.com>
2021-04-22 15:52:23 -0300

    lint

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,20 +33,20 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
-    // // ESCREVA SEUS TESTES ABAIXO:
-    // // Teste que o retorno da função é um array.
-    // assert.strictEqual(Array.isArray(productDetails()), true);
-    // // Teste que o array retornado pela função contém dois itens dentro.
-    // assert.strictEqual(productDetails('Alcool gel', 'Máscara').length, 2);
-    // // Teste que os dois itens dentro do array retornado pela função são objetos.
-    // assert.strictEqual(typeof(productDetails('Alcool gel', 'Máscara')[0]), 'object');
-    // assert.strictEqual(typeof(productDetails('Alcool gel', 'Máscara')[1]), 'object');
-    // // Teste que os dois objetos são diferentes entre si.
-    // assert.strictEqual(productDetails()[0] !== productDetails()[1], true);
-    // // (Difícil) Teste que os dois productIds terminam com 123.
-    // productDetails('Alcool gel', 'Máscara')[0].details.productId.includes('123'), true;
-    // productDetails('Alcool gel', 'Máscara')[1].details.productId.includes('123'), true;
-    // // .includes estudado em https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/contains
+    // assert.fail();
+    // ESCREVA SEUS TESTES ABAIXO:
+    // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails()), true);
+    // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara').length, 2);
+    // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof (productDetails('Alcool gel', 'Máscara')[0]), 'object');
+    assert.strictEqual(typeof (productDetails('Alcool gel', 'Máscara')[1]), 'object');
+    // Teste que os dois objetos são diferentes entre si.
+    assert.strictEqual(productDetails()[0] !== productDetails()[1], true);
+    // (Difícil) Teste que os dois productIds terminam com 123.
+    productDetails('Alcool gel', 'Máscara')[0].details.productId.includes('123'), true;
+    productDetails('Alcool gel', 'Máscara')[1].details.productId.includes('123'), true;
+    // .includes estudado em https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/contains
   });
 });
