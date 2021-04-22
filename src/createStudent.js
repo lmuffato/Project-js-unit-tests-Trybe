cc8ac4b91acdbba04c03d243f9568e6248a706cf
Victor Ricco <victor.ricco001@gmail.com>
2021-04-22 15:03:50 -0300

    possivelmente feito ate o requisito 6

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const student = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+
+  return student;
+};
 
 module.exports = createStudent;
