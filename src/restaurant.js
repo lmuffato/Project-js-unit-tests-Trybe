a6f2524a6a0bb3553333c3aedce1db2b522a51a1
Wendell Costa <wendellcosta9141@gmail.com>
2021-04-23 01:04:57 -0300

    [Requisito 9 - Refatorado]

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,28 +1,23 @@
-const menu = {};
+const createMenu = (object) => {
+  const consumption = [];
+  return {
+    fetchMenu: () => object,
+    consumption,
+    order: (str) => consumption.push(str),
+    pay: () => {
+      const { food, drink } = object;
+      const foodKeys = Object.keys(food);
+      const drinkKeys = Object.keys(drink);
+      let total = 0;
 
-const orderSum = () => {
-  let sum = 0;
-  const { drink } = menu.fetchMenu;
-  const { food } = menu.fetchMenu;
-  for (let index = 0; index < menu.consumption.length; index += 1) {
-    if (Object.keys(drink).includes(menu.consumption[index])) {
-      sum += drink[menu.consumption[index]];
-    }
-    if (Object.keys(food).includes(menu.consumption[index])) {
-      sum += food[menu.consumption[index]];
-    }
-  }
-  return sum * 1.1;
-};
-
-const createMenu = (param) => {
-  menu.fetchMenu = param;
-  menu.consumption = [];
-  menu.order = (request) => {
-    menu.consumption.push(request);
+      for (let i = 0; i < consumption.length; i += 1) {
+        const order = consumption[i];
+        if (foodKeys.includes(order)) total += food[order];
+        if (drinkKeys.includes(order)) total += drink[order];
+      }
+      return total * 1.1;
+    },
   };
-  menu.pay = orderSum;
-  return menu;
 };
 
 module.exports = createMenu;
