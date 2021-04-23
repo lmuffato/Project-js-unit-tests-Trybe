f67bc7ec8c29193b0aa834c65060f9d5f028946f
MZTBTG <mztbtg@hotmail.com>
2021-04-23 01:17:51 -0300

    Correção de exercício 1

--- tests/myCounter.spec.js
+++ tests/myCounter.spec.js
@@ -18,6 +18,6 @@ const myCounter = require('../src/myCounter');
 describe('#myCounter', () => {
   it('guarantees the function behaves as expected', () => {
     const expectedOutput = [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
-    assert.deepStrictEqual(myCounter(), expectedOutput);
+    assert.StrictEqual(myCounter(), expectedOutput);
   });
 });
