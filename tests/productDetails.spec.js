b9f0a990bc6cd3e6610748c69b7201431e1a036c
Djaniza Vasques <djanizav@gmail.com>
2021-04-23 07:54:18 -0300

    Adiciona requisito 4

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,14 +31,17 @@ const productDetails = require('../src/productDetails');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('#productDetails', () => {
+/* describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
     assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof (productDetails(produto, prod2)), 'array');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails.name.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
     // Teste que os dois objetos são diferentes entre si.
     // (Difícil) Teste que os dois productIds terminam com 123.
   });
 });
+*/
