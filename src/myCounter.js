97919d9a14586fe46c5fb02241d01d6b0fffea3f
Djaniza Vasques <djanizav@gmail.com>
2021-04-23 08:34:00 -0300

    Corrige requisito 8

--- src/myCounter.js
+++ src/myCounter.js
@@ -14,8 +14,8 @@ const myCounter = () => {
   let myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for ( let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let counterIndex = 2; counterIndex <= 3; counterIndex += 1) {
+      myArray.push(counterIndex);
     }
   }
   return myArray;
