6ab2746fad029f75143ded3f72784712c4e42575
Thiago <thiagodesouza.dev@gmail.com>
2021-04-23 12:51:35 -0300

    atividade 9

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -91,7 +91,7 @@ describe('#createMenu', () => {
     // objetoRetornado.order("coxinha");
     // objetoRetornado.consumption // Retorno: ["coxinha"]
     // ```
-
+    // assert.deepStrictEqual()
     // Agora faça o PASSO 3 no arquivo `src/restaurant.js`.
     // --------------------------------------------------------------------------------------
     // TESTE 6: Verifique que as três orders seguintes, de bebidas e comidas mescladas, somam três itens no array `objetoRetornado.consumption` conforme os itens pedidos.
