56c552851ada3ac1a6c1c5cdc55f562554d42eab
Matheus Domingos <matheusyurisd@gmail.com>
2021-07-08 17:29:59 -0300

    Update myCounter.spec.js

--- tests/myCounter.spec.js
+++ tests/myCounter.spec.js
@@ -15,7 +15,7 @@ const myCounter = require('../src/myCounter');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('#myCounter', () => {
+describe('8 - Implemente a função `myCounter`', () => {
   it('guarantees the function behaves as expected', () => {
     const expectedOutput = [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
     assert.deepStrictEqual(myCounter(), expectedOutput);
